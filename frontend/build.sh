f0720e1e370a0d7f4cd4a5906dd43a65f6d4492a
wo-o29 <woogur29@gmail.com>
2025-07-14 22:10:08 +0900

    chore: 빌드 명령어 수정

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,3 +1,12 @@
+# 스크립트 실행 중 하나라도 에러가 발생하면 전체 스크립트 실행이 중단되어 문제를 빠르게 감지하고 디버깅 가능
+set -e
+
+# output 폴더 비우기
+rm -rf output
 mkdir -p output
-cp -R ../2025-moaon/* ./output
-cp -R ./output ../2025-moaon/
+
+# .github 복사
+cp -R ../.github ./output/
+
+# rsync로 frontend 전체 복사
+rsync -av --exclude=output --exclude=.git --exclude=node_modules --exclude=.vscode ./ ./output/frontend/
