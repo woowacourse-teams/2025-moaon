eb3ce020091c1df2ae7c2d7610fec4dcfc915e73
wo-o29 <woogur29@gmail.com>
2025-07-17 22:05:07 +0900

    333

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<h1>모아온 테스트 - 10:03 </h1>} />
+      <Route path="/" element={<h1>모아온 테스트 - 10:05 </h1>} />
     </Routes>
   );
 }
