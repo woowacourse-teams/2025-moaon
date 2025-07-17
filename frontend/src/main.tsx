83407d72779c879bedb471561a31661d38e6d032
mlnwns <129190157+mlnwns@users.noreply.github.com>
2025-07-17 11:25:55 +0900

    refactor: reset css를 emotion을 사용해 ts 파일로 변경 (#16)
    * refactor: reset css 파일을 ts 파일로 변경
    
    * refactor: 사용하지 않는 loader 제거
    
    * chore: biome 설치

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,16 @@
+import { Global } from "@emotion/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./styles/reset.css";
 import { BrowserRouter } from "react-router";
+import App from "./App";
+import { resetStyle } from "./styles/reset.styled";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <StrictMode>
     <BrowserRouter>
+      <Global styles={resetStyle} />
       <App />
     </BrowserRouter>
   </StrictMode>,
