b74da24fb79526c5976a43539ef7d82d1f49250e
wo-o29 <woogur29@gmail.com>
2025-07-18 15:35:02 +0900

    feat: Header 컴포넌트 구현

---
+++ frontend/src/shared/components/Header/Header.styled.ts
@@ -0,0 +1,25 @@
+import styled from "@emotion/styled";
+
+export const Header = styled.header`
+  width: 100%;
+  position: sticky;
+  top: 0;
+  background-color: #fff;
+  padding: 12px 0;
+  border-bottom: 1px solid #d2d2d2;
+`;
+
+export const HeaderBox = styled.div`
+  width: 80rem;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  
+`;
+
+export const Flex = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
