82a90b21a5575436a4ca9a36ec589b7b42609e08
wo-o29 <woogur29@gmail.com>
2025-07-19 17:41:48 +0900

    123123123

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,17 @@ function App() {
         path="/"
         element={<h1>모아온 테스트 - 7월 19일 토요일 17:22</h1>}
       />
+      <Route
+        path="*"
+        element={
+          <h1>
+            에러 페이지 <br />
+            <br />
+            <br />
+            <a href="/">메인 페이지로 돌아가기</a>
+          </h1>
+        }
+      />
     </Routes>
   );
 }
