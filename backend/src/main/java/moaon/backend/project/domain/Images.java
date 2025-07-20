2ab730ae39958c3c94e1b3f10b609b7f81d01ef0
yesjuhee <juhee0924@g.skku.edu>
2025-07-21 01:16:07 +0900

    refactor: Images 일급컬렉션에 방어적 복사 로직 추가 및 ``@Getter`, `@EqualsAndHashCode` 어노테이션 추가

--- backend/src/main/java/moaon/backend/project/domain/Images.java
+++ backend/src/main/java/moaon/backend/project/domain/Images.java
@@ -2,18 +2,27 @@ package moaon.backend.project.domain;
 
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Embeddable;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Getter
+@EqualsAndHashCode
+@ToString
 public class Images {
 
     @ElementCollection
     private List<String> urls;
+
+    public Images(List<String> urls) {
+        this.urls = new ArrayList<>(urls);
+    }
+
+    public List<String> getUrls() {
+        return List.copyOf(urls);
+    }
 }
