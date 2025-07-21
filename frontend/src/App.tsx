254b1412a96318b6927f465f20ae76d512d62441
wo-o29 <woogur29@gmail.com>
2025-07-21 12:34:21 +0900

    feat: 프로덕션 배포 테스트

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
-import { Route, Routes } from "react-router";
+import { Link, Route, Routes } from "react-router";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<h1>모아온 프로덕션 배포</h1>} />
       <Route
-        path="/"
-        element={<h1>모아온 테스트 - 2025년 07월 14일 10:08</h1>}
+        path="*"
+        element={
+          <>
+            <h1>존재 하지 않는 페이지 입니다.</h1>
+            <br />
+            <br />
+            <br />
+            <Link to="/">메인 페이지로 이동</Link>
+          </>
+        }
       />
     </Routes>
   );
