3619c400acb3afecac9c56030c6f1a3bb8547216
wo-o29 <woogur29@gmail.com>
2025-07-22 17:49:32 +0900

    refactor: SearchBar 공용 컴포넌트로 변경

--- frontend/src/shared/components/Header/Header.tsx
+++ frontend/src/shared/components/Header/Header.tsx
@@ -1,8 +1,8 @@
 import Logo from "@assets/icons/logo.svg";
+import SearchBar from "../SearchBar/SearchBar";
 import * as S from "./Header.styled";
 import NavBar from "./NavBar/NavBar";
 import RegisterProjectButton from "./RegisterProjectButton/RegisterProjectButton";
-import SearchBar from "./SearchBar/SearchBar";
 
 function Header() {
   return (
@@ -13,7 +13,11 @@ function Header() {
           <NavBar />
         </S.Wrap>
         <S.Wrap>
-          <SearchBar />
+          <SearchBar
+            width="fixed"
+            shape="rounded"
+            icon={{ size: 16, position: "left" }}
+          />
           <RegisterProjectButton />
         </S.Wrap>
       </S.HeaderBox>
---
+++ frontend/src/shared/components/SearchBar/SearchBar.styled.ts
@@ -0,0 +1,36 @@
+import styled from "@emotion/styled";
+
+export const SearchBox = styled.label<{
+  position: "left" | "right";
+  shape: "default" | "rounded";
+  width: "full" | "fixed";
+}>`
+  width: ${({ width }) => (width === "full" ? "100%" : "17.1875rem")};
+  border: 1px solid #6b7684;
+  border-radius: ${({ shape }) => (shape === "rounded" ? "20px" : "12px")};
+  padding: 0.75rem;
+  display: flex;
+  flex-direction: ${({ position }) =>
+    position === "left" ? "row" : "row-reverse"};
+  align-items: center;
+  gap: 0.5rem;
+  justify-content: space-between;
+
+  &:focus-within {
+    border-color: #007bff;
+    box-shadow: 0 0 0 2px rgba(0, 123, 255, 0.15);
+  }
+`;
+
+export const SearchInput = styled.input`
+  width: 100%;
+
+  &::placeholder {
+    color: #8b95a1;
+  }
+`;
+
+export const SearchIcon = styled.img<{ size: number }>`
+  width: ${({ size }) => size}px;
+  aspect-ratio: 1/1;
+`;
