de164949d6e3bdebad75b2417c6b01b40b7d61e5
mlnwns <mlnwns@naver.com>
2025-07-24 11:02:42 +0900

    style: 글로벌 레이아웃 너비 변경

--- frontend/src/shared/components/Layout/GlobalLayout.styled.ts
+++ frontend/src/shared/components/Layout/GlobalLayout.styled.ts
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 
 export const GlobalLayout = styled.div`
-  max-width: 75rem;
+  max-width: 80rem;
   margin: 2.25rem auto;
   height: calc(100dvh - 5.125rem);
 `;
