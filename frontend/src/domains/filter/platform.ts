de13fc0b8902b8830625ad76c3cba674cf364579
mlnwns <mlnwns@naver.com>
2025-07-25 11:38:11 +0900

    chore: 플랫폼 아이콘 S3 경로로 변경

--- frontend/src/domains/filter/platform.ts
+++ frontend/src/domains/filter/platform.ts
@@ -1,19 +1,23 @@
 export const PLATFORM_ICON_MAP = {
   web: {
     label: "Web",
-    imgUrl: "https://cdn-icons-png.flaticon.com/512/1006/1006771.png",
+    imgUrl:
+      "https://techcourse-project-2025.s3.ap-northeast-2.amazonaws.com/moaon/platforms/web.svg",
   },
   desktop: {
     label: "Desktop",
-    imgUrl: "https://icon.icepanel.io/Technology/svg/TypeScript.svg",
+    imgUrl:
+      "https://techcourse-project-2025.s3.ap-northeast-2.amazonaws.com/moaon/platforms/desktop.svg",
   },
   ios: {
     label: "iOS",
-    imgUrl: "https://icon.icepanel.io/Technology/svg/React.svg",
+    imgUrl:
+      "https://techcourse-project-2025.s3.ap-northeast-2.amazonaws.com/moaon/platforms/ios.svg",
   },
   android: {
     label: "Android",
-    imgUrl: "https://icon.icepanel.io/Technology/svg/NextJS.svg",
+    imgUrl:
+      "https://techcourse-project-2025.s3.ap-northeast-2.amazonaws.com/moaon/platforms/android.svg",
   },
 } as const;
 
