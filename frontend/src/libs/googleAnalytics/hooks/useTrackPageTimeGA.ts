a593defec8fee8c840703c785eb717fdcb1a3e78
jin123457 <jinsol9909@gmail.com>
2025-07-31 14:10:06 +0900

    feat: ga4 머문 시간 및 초기 값 설정 구현

---
+++ frontend/src/libs/googleAnalytics/hooks/useTrackPageTimeGA.ts
@@ -0,0 +1,42 @@
+import { useEffect, useRef } from "react";
+import ReactGA from "react-ga4";
+import { useLocation } from "react-router";
+
+const normalizePath = (pathname: string) => {
+  if (/^\/detail\/\d+/.test(pathname)) {
+    return "/detail/[id]";
+  }
+
+  return pathname;
+};
+
+const useTrackPageTimeGA = () => {
+  const location = useLocation();
+  const prevPathRef = useRef(location.pathname);
+  const startTimeRef = useRef(Date.now());
+  const isFirstRunRef = useRef(true);
+
+  useEffect(() => {
+    if (isFirstRunRef.current) {
+      isFirstRunRef.current = false;
+      return;
+    }
+
+    const now = Date.now();
+    const stayTime = now - startTimeRef.current;
+
+    if (prevPathRef.current !== location.pathname) {
+      ReactGA.event({
+        category: "User Engagement",
+        action: "time_stay_per_page",
+        label: normalizePath(prevPathRef.current),
+        value: stayTime / 1000,
+      });
+
+      prevPathRef.current = location.pathname;
+      startTimeRef.current = now;
+    }
+  }, [location.pathname]);
+};
+
+export default useTrackPageTimeGA;
