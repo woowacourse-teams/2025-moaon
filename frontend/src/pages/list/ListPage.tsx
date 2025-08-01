f09066ad49355b2960efe9aee17049fd1308b1ef
wo-o29 <woogur29@gmail.com>
2025-08-01 14:05:09 +0900

    refactor: 필터 초기화 로직 수정

--- frontend/src/pages/list/ListPage.tsx
+++ frontend/src/pages/list/ListPage.tsx
@@ -1,14 +1,10 @@
 import CloseIcon from "@assets/icons/close.svg";
 import ResetIcon from "@assets/icons/reset.svg";
 import { CATEGORY_MAP, type CategoryKey } from "@domains/filter/category";
-import { PLATFORM_ICON_MAP, type PlatformKey } from "@domains/filter/platform";
 import {
   TECH_STACK_ICON_MAP,
   type TechStackKey,
 } from "@domains/filter/techStack";
-import { useState } from "react";
-import { flushSync } from "react-dom";
-import { useSearchParams } from "react-router";
 import CardList from "./CardList/CardList";
 import FilterContainer from "./FilterContainer/FilterContainer";
 import { useFilterParams } from "./hooks/useFilterParams";
@@ -16,47 +12,37 @@ import useProjectList from "./hooks/useProjectList";
 import * as S from "./ListPage.styled";
 import SortList from "./SortList/SortList";
 
-const getSelectedItems = (searchParams: URLSearchParams) => {
-  const techStacks = (searchParams.get("techStacks")?.split(",") ||
-    []) as TechStackKey[];
-  const platforms = (searchParams.get("platforms")?.split(",") ||
-    []) as PlatformKey[];
-  const categories = (searchParams.get("categories")?.split(",") ||
-    []) as CategoryKey[];
-
-  return {
-    techStacks: techStacks.map((item) => ({
+const converterFilterItems = (
+  techStacks: TechStackKey[],
+  categories: CategoryKey[],
+) => {
+  return [
+    ...techStacks.map((item) => ({
       key: "techStacks",
       label: TECH_STACK_ICON_MAP[item].label,
       value: item,
     })),
-    platforms: platforms.map((item) => ({
-      key: "platforms",
-      label: PLATFORM_ICON_MAP[item].label,
-      value: item,
-    })),
-    categories: categories.map((item) => ({
+    ...categories.map((item) => ({
       key: "categories",
       label: CATEGORY_MAP[item].label,
       value: item,
     })),
-  };
+  ];
 };
 
 function ListPage() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [isFilterRender, setIsFilterRender] = useState(true);
+  const { techStacks, categories, deleteFilter, resetFilter } =
+    useFilterParams();
+  const selectedFilterItems = converterFilterItems(techStacks, categories);
   const { refetch } = useProjectList();
-  const { deleteFilter } = useFilterParams();
-  const { techStacks, categories, platforms } = getSelectedItems(searchParams);
 
   const handleFilterResetButtonClick = () => {
-    flushSync(() => setIsFilterRender(false));
+    resetFilter();
+    refetch();
+  };
 
-    const sortParams = searchParams.get("sort");
-    sortParams ? setSearchParams({ sort: sortParams }) : setSearchParams({});
-
-    setIsFilterRender(true);
+  const handleUnselectFilterItem = (key: string, value: string) => {
+    deleteFilter(key, value);
     refetch();
   };
 
@@ -64,7 +50,7 @@ function ListPage() {
     <S.Main>
       <S.Box>
         <S.Wrap>
-          {isFilterRender && <FilterContainer />}
+          <FilterContainer />
           <S.ResetButton type="button" onClick={handleFilterResetButtonClick}>
             <S.ResetIcon src={ResetIcon} alt="필터 초기화 아이콘" />
             필터 초기화
@@ -74,20 +60,15 @@ function ListPage() {
       </S.Box>
       <S.SelectedBox>
         <S.SelectedList>
-          {[...techStacks, ...categories, ...platforms].map(
-            ({ key, value, label }) => (
-              <S.SelectedItem
-                key={value}
-                onClick={() => {
-                  deleteFilter(key, value);
-                  refetch();
-                }}
-              >
-                {label}
-                <img src={CloseIcon} alt="필터 삭제" />
-              </S.SelectedItem>
-            ),
-          )}
+          {selectedFilterItems.map(({ key, value, label }) => (
+            <S.SelectedItem
+              key={value}
+              onClick={() => handleUnselectFilterItem(key, value)}
+            >
+              {label}
+              <img src={CloseIcon} alt="필터 삭제" />
+            </S.SelectedItem>
+          ))}
         </S.SelectedList>
       </S.SelectedBox>
       <CardList />
--- frontend/src/pages/list/hooks/useFilterParams.ts
+++ frontend/src/pages/list/hooks/useFilterParams.ts
@@ -1,25 +1,37 @@
+import type { CategoryKey } from "@domains/filter/category";
+import type { TechStackKey } from "@domains/filter/techStack";
 import useSearchParams from "@shared/hooks/useSearchParams";
+import { useSearchParams as useReactRouterSearchParams } from "react-router";
 
 export const useFilterParams = () => {
-  const techStacks = useSearchParams({ key: "techStacks", mode: "multi" });
-  const platforms = useSearchParams({ key: "platforms", mode: "multi" });
-  const categories = useSearchParams({ key: "categories", mode: "multi" });
+  const [searchParams, setSearchParams] = useReactRouterSearchParams();
+  const techStackParams = useSearchParams({ key: "techStacks", mode: "multi" });
+  const categoryParams = useSearchParams({ key: "categories", mode: "multi" });
 
   const deleteFilter = (key: string, value: string) => {
     switch (key) {
       case "techStacks":
-        techStacks.update(value);
-        break;
-      case "platforms":
-        platforms.update(value);
+        techStackParams.update(value);
         break;
       case "categories":
-        categories.update(value);
+        categoryParams.update(value);
         break;
       default:
         throw new Error(`Unknown filter key: ${key}`);
     }
   };
 
-  return { deleteFilter };
+  const resetFilter = () => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.delete("categories");
+    params.delete("techStacks");
+    setSearchParams(params);
+  };
+
+  return {
+    techStacks: techStackParams.get() as TechStackKey[],
+    categories: categoryParams.get() as CategoryKey[],
+    deleteFilter,
+    resetFilter,
+  };
 };
