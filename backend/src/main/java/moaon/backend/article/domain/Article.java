ef8552d2eec5539193d7ed19922afa0ef8e56c7d
(minjae8563) <(minjae8563@naver.com)>
2025-08-01 14:50:07 +0900

    feat: Article 엔티티 구현

---
+++ backend/src/main/java/moaon/backend/article/domain/Article.java
@@ -0,0 +1,59 @@
+package moaon.backend.article.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import moaon.backend.global.domain.BaseTimeEntity;
+import moaon.backend.project.domain.Project;
+import moaon.backend.techStack.domain.TechStack;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@EqualsAndHashCode(of = "id", callSuper = false)
+@ToString
+public class Article extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String summary;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String articleUrl;
+
+    @Column(nullable = false)
+    private int clicks;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private ArticleCategory category;
+
+    @ManyToMany
+    private List<TechStack> techStacks;
+}
