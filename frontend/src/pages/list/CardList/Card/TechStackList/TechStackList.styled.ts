34e3f58a08902c4f53f1796912233ed06f5429ae
wo-o29 <woogur29@gmail.com>
2025-08-01 14:54:47 +0900

    refactor: 프로젝트 카드 기술스택 스타일 수정

--- frontend/src/pages/list/CardList/Card/TechStackList/TechStackList.styled.ts
+++ frontend/src/pages/list/CardList/Card/TechStackList/TechStackList.styled.ts
@@ -1,18 +1,25 @@
 import styled from "@emotion/styled";
 
 export const TechStackList = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
   padding: 0.25rem 0 0;
-  gap: 0.75rem;
-  min-height: 2.625rem;
+  gap: 0.25rem;
+  height: 1.25rem;
+  overflow: hidden;
 `;
 
-export const AdditionalCount = styled.div`
-  border: 1px solid #d2d2d2;
+export const TechStack = styled.li`
+  color: #7f7f7f;
+  font-size: 13px;
+  font-weight: 500;
+`;
+
+export const Separator = styled.div`
+  background-color: #7f7f7f;
+  width: 2px;
+  height: 2px;
   border-radius: 50%;
-  padding: 0.5rem;
   display: flex;
-  align-items: center;
-  justify-content: center;
 `;
