4c40b099dc9a8216b6c7a6d0f92f02c9e7a3d09c
wo-o29 <woogur29@gmail.com>
2025-08-01 19:21:36 +0900

    refactor: TechStackList 스타일 px to rem

--- frontend/src/pages/list/CardList/Card/TechStackList/TechStackList.styled.ts
+++ frontend/src/pages/list/CardList/Card/TechStackList/TechStackList.styled.ts
@@ -5,21 +5,21 @@ export const TechStackList = styled.ul`
   flex-wrap: wrap;
   align-items: center;
   padding: 0.25rem 0 0;
-  gap: 0.25rem;
+  gap: 0.375rem;
   height: 1.25rem;
   overflow: hidden;
 `;
 
 export const TechStack = styled.li`
   color: #7f7f7f;
-  font-size: 13px;
+  font-size: 0.8125rem;
   font-weight: 500;
 `;
 
 export const Separator = styled.div`
   background-color: #7f7f7f;
-  width: 2px;
-  height: 2px;
+  width: 0.125rem;
+  height: 0.125rem;
   border-radius: 50%;
   display: flex;
 `;
