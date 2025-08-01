d802f2131ebd92cd3f4ebdc43882fc1b08c97c37
wo-o29 <woogur29@gmail.com>
2025-08-01 19:34:49 +0900

    refactor: 필터 초기화 버튼 디자인 수정

--- frontend/src/pages/list/ListPage.styled.ts
+++ frontend/src/pages/list/ListPage.styled.ts
@@ -7,8 +7,8 @@ export const Main = styled.main`
 export const Box = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: center;
   width: 100%;
+  padding-bottom: 2rem;
 `;
 
 export const Wrap = styled.div`
@@ -21,7 +21,7 @@ export const ResetButton = styled.button`
   align-items: center;
   gap: 0.5rem;
   border: 1px solid #d2d2d2;
-  border-radius: 16px;
+  border-radius: 0.5rem;
   padding: 0.5rem 1rem;
   font-size: 1rem;
   color: #474747ff;
@@ -29,38 +29,13 @@ export const ResetButton = styled.button`
   &:hover {
     background-color: #f5f5f5;
     transition: background-color 0.5s ease;
+
+    & > img {
+      transform: rotate(180deg);
+      transition: transform 0.5s ease;
+    }
   }
 `;
 export const ResetIcon = styled.img`
-  width: 1.25rem;
-`;
-
-export const SelectedBox = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  margin: 0.25rem 0;
-  min-height: 68px;
-`;
-
-export const SelectedList = styled.ul`
-  display: flex;
-  gap: 0.5rem;
-  flex-wrap: wrap;
-`;
-
-export const SelectedItem = styled.li`
-  padding: 0.5rem 0.5rem 0.5rem 0.75rem;
-  background-color: #f0f0f0;
-  border-radius: 16px;
-  font-size: 0.875rem;
-  color: #333;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  gap: 0.25rem;
-
-  &:not(:last-child) {
-    margin-right: 0.5rem;
-  }
+  width: 1rem;
 `;
--- frontend/src/pages/list/ListPage.tsx
+++ frontend/src/pages/list/ListPage.tsx
@@ -1,10 +1,4 @@
-import CloseIcon from "@assets/icons/close.svg";
-import ResetIcon from "@assets/icons/reset.svg";
-import { CATEGORY_MAP, type CategoryKey } from "@domains/filter/category";
-import {
-  TECH_STACK_ICON_MAP,
-  type TechStackKey,
-} from "@domains/filter/techStack";
+import resetIcon from "@assets/icons/reset.svg";
 import CardList from "./CardList/CardList";
 import FilterContainer from "./FilterContainer/FilterContainer";
 import { useFilterParams } from "./hooks/useFilterParams";
@@ -12,37 +6,12 @@ import useProjectList from "./hooks/useProjectList";
 import * as S from "./ListPage.styled";
 import SortList from "./SortList/SortList";
 
-const converterFilterItems = (
-  techStacks: TechStackKey[],
-  categories: CategoryKey[],
-) => {
-  return [
-    ...techStacks.map((item) => ({
-      key: "techStacks",
-      label: TECH_STACK_ICON_MAP[item].label,
-      value: item,
-    })),
-    ...categories.map((item) => ({
-      key: "categories",
-      label: CATEGORY_MAP[item].label,
-      value: item,
-    })),
-  ];
-};
-
 function ListPage() {
-  const { techStacks, categories, deleteFilter, resetFilter } =
-    useFilterParams();
-  const selectedFilterItems = converterFilterItems(techStacks, categories);
+  const { resetFilter } = useFilterParams();
   const { refetch } = useProjectList();
 
   const handleFilterResetButtonClick = () => {
     resetFilter();
-    refetch();
-  };
-
-  const handleUnselectFilterItem = (key: string, value: string) => {
-    deleteFilter(key, value);
     refetch();
   };
 
@@ -52,25 +21,11 @@ function ListPage() {
         <S.Wrap>
           <FilterContainer />
           <S.ResetButton type="button" onClick={handleFilterResetButtonClick}>
-            <S.ResetIcon src={ResetIcon} alt="필터 초기화 아이콘" />
-            필터 초기화
+            <S.ResetIcon src={resetIcon} alt="필터 초기화" />
           </S.ResetButton>
         </S.Wrap>
         <SortList />
       </S.Box>
-      <S.SelectedBox>
-        <S.SelectedList>
-          {selectedFilterItems.map(({ key, value, label }) => (
-            <S.SelectedItem
-              key={value}
-              onClick={() => handleUnselectFilterItem(key, value)}
-            >
-              {label}
-              <img src={CloseIcon} alt="필터 삭제" />
-            </S.SelectedItem>
-          ))}
-        </S.SelectedList>
-      </S.SelectedBox>
       <CardList />
     </S.Main>
   );
