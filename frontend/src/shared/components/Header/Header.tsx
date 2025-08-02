7384086933305a8381ed6ca51d333996b2c33fa0
wo-o29 <woogur29@gmail.com>
2025-08-02 23:00:58 +0900

    refactor: 헤더 서치바 제거

--- frontend/src/shared/components/Header/Header.tsx
+++ frontend/src/shared/components/Header/Header.tsx
@@ -1,6 +1,5 @@
 import Logo from "@assets/icons/logo.svg";
 import { Link } from "react-router";
-import SearchBar from "../SearchBar/SearchBar";
 import * as S from "./Header.styled";
 import NavBar from "./NavBar/NavBar";
 import RegisterProjectButton from "./RegisterProjectButton/RegisterProjectButton";
@@ -16,12 +15,6 @@ function Header() {
           <NavBar />
         </S.Wrap>
         <S.Wrap>
-          <SearchBar
-            width="fixed"
-            shape="rounded"
-            icon={{ size: 16, position: "left" }}
-            placeholder="프로젝트를 검색해 보세요"
-          />
           <RegisterProjectButton />
         </S.Wrap>
       </S.HeaderBox>
