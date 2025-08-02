38468b5b1fc9f90b825e1e7d931a153f239b0b41
yesjuhee <juhee0924@g.skku.edu>
2025-08-02 16:36:36 +0900

    feat: Resource Not Found 관련 예외 처리 및 ErrorCode 개선
    - `RESOURCE_NOT_FOUND` ErrorCode 추가 및 기존 ErrorCode 구조 확장
    - `GlobalExceptionHandler`에 `NoResourceFoundException` 처리 추가
    - 예외 발생 시 요청 URI, HTTP 메서드, 에러 이름 및 상세 정보 로그 작성

--- backend/src/main/java/moaon/backend/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/moaon/backend/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package moaon.backend.global.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import moaon.backend.global.exception.custom.CustomException;
 import moaon.backend.global.exception.custom.ErrorCode;
@@ -7,6 +8,9 @@ import moaon.backend.global.exception.dto.ErrorResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @RestControllerAdvice
@@ -19,9 +23,36 @@ public class GlobalExceptionHandler {
                 .body(ErrorResponse.from(e.getErrorCode()));
     }
 
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoResourceFoundException(NoResourceFoundException e) {
+        ErrorCode errorCode = ErrorCode.RESOURCE_NOT_FOUND;
+
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        String requestURI = "unknown";
+        String httpMethod = "unknown";
+
+        if (requestAttributes != null) {
+            HttpServletRequest request = requestAttributes.getRequest();
+            requestURI = request.getRequestURI();
+            httpMethod = request.getMethod();
+        }
+
+        log.warn("[{}] Method: {}, Path: {}, Error ID: {}, Detail: {}",
+                errorCode.getName(),
+                httpMethod,
+                requestURI,
+                errorCode.getId(),
+                e.getMessage()
+        );
+        
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(ErrorResponse.from(errorCode));
+    }
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleUncaughtException(Exception e) {
-        log.error("[Unhandled Exception]: {}", e.getMessage(), e);
+    public ResponseEntity<ErrorResponse> handleUncaughtException(Exception e) { // TODO 위에 맞춰서 개선
+        log.error("[Unhandled Exception] {}", e.getMessage(), e);
         ErrorCode unknownError = ErrorCode.UNKNOWN;
         return ResponseEntity
                 .status(unknownError.getHttpStatus())
