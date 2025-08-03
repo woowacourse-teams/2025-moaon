e8d4fffd4487ef3b9fa272d8ac91bf81bdcbb6f5
Minuring <144205824+Minuring@users.noreply.github.com>
2025-08-03 15:01:32 +0900

    chore: 모든 경로에 대해 origin을 허용하도록 변경
    Co-authored-by: 노주희 <juhee0924@g.skku.edu>

--- backend/src/main/java/moaon/backend/global/config/WebConfig.java
+++ backend/src/main/java/moaon/backend/global/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/projects/**")
+        registry.addMapping("/**")
             .allowedOrigins(
                 "http://localhost:3000",
                 "https://moaon-dev.vercel.app",
