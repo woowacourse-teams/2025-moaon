6a694b7f3d46456b2d3556fc00bb51c7fd0fc01a
Minuring <mw9505@gmail.com>
2025-08-03 12:38:13 +0900

    feat: preview 배포 주소 origin 허용

--- backend/src/main/java/moaon/backend/global/config/WebConfig.java
+++ backend/src/main/java/moaon/backend/global/config/WebConfig.java
@@ -10,14 +10,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/projects/**")
-                .allowedOrigins(
-                        "http://localhost:3000",
-                        "https://moaon-dev.vercel.app",
-                        "https://d2ye9egv48bag1.cloudfront.net"
-                )
-                .allowedMethods("GET")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+            .allowedOrigins(
+                "http://localhost:3000",
+                "https://moaon-dev.vercel.app",
+                "https://d2ye9egv48bag1.cloudfront.net",
+                "https://moaon-preview-*.vercel.app"
+            )
+            .allowedMethods("GET")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600);
     }
 }
