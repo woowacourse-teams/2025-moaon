3ce8dc2f0176233591cd92f9e326c57ec81c8246
jin123457 <jinsol9909@gmail.com>
2025-08-04 17:15:44 +0900

    feat: 아티클 페이지 헤더 UI 추가

--- frontend/src/pages/article/ArticlePage.tsx
+++ frontend/src/pages/article/ArticlePage.tsx
@@ -1,13 +1,23 @@
+import ArticleSearchBar from "./ArticleSearchBar/ArticleSearchBar";
 import * as S from "./AticlePage.styled";
 import CardList from "./CardList/CardList";
 import TagList from "./TagList/TagList";
 
 function ArticlePage() {
   return (
-    <S.ArticlePageContainer>
+    <S.Main>
+      <S.MainBox>
+        <S.TitleBox>
+          <S.MainTitle>프로젝트 탐색</S.MainTitle>
+          <S.MainDescription>
+            다양한 개발자들의 프로젝트를 탐색하고 학습하세요
+          </S.MainDescription>
+        </S.TitleBox>
+        <ArticleSearchBar />
+      </S.MainBox>
       <CardList />
       <TagList />
-    </S.ArticlePageContainer>
+    </S.Main>
   );
 }
 
