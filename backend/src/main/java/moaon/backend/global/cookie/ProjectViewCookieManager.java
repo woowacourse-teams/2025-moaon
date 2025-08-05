9fc675a481e449cf759a02b8d628a05782d0b612
eueo8259 <162389416+eueo8259@users.noreply.github.com>
2025-08-05 14:21:23 +0900

    feat(BE): 프로젝트 조회수 기능 고도화 (#125)
    * feat : ProjectViewCookieManager 구현
    
    * feat: 프로젝트 조회 쿠키 매니저 빈 수동 등록을 위한 설정 클래스 추가
    
    * feat: 쿠키 적용 이후 조회수 증가 기능 구현
    
    * test: 조회수 증가 기능 테스트 구현 및 수정
    
    * style: 코딩 컨벤션 수정
    
    * refactor: increaseViewsCount 로직 수정
    
    * refactor: createOrUpdateCookie 메서드 분리 및 try-catch 예외 처리 세분화
    
    * refactor: ViewedProjects 일급 컬렉션 도입 및 책임 이동
    
    * test: 필요없다 판단되는 test 삭제
    
    * refactor: 클래스 명 및 변수명 수정
    
    * chore: 로그 컨벤션에 맞도록 추가
    
    * refactor: 의미 있는 상수값 표현으로 주석 제거
    
    * style: 포매팅 수정
    
    * style: 개행 수정

---
+++ backend/src/main/java/moaon/backend/global/cookie/ProjectViewCookieManager.java
@@ -0,0 +1,109 @@
+package moaon.backend.global.cookie;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ProjectViewCookieManager {
+
+    private static final String COOKIE_NAME = "viewed_projects";
+    private static final String COOKIE_PATH = "/projects";
+
+    public ProjectViewTimes extractViewedMap(HttpServletRequest request) {
+        Cookie cookie = getCookie(request);
+        if (cookie == null) {
+            return ProjectViewTimes.empty();
+        }
+        String decodedCookieValue = URLDecoder.decode(cookie.getValue(), StandardCharsets.UTF_8);
+
+        ProjectViewTimes projectViewTimes = ProjectViewTimes.from(decodedCookieValue);
+        projectViewTimes.removeExpiredEntries(getCurrentTimeSeconds());
+        return projectViewTimes;
+    }
+
+    public boolean isViewCountIncreasable(long projectId, ProjectViewTimes projectViewTimes) {
+        long currentTimeSeconds = getCurrentTimeSeconds();
+        return projectViewTimes.isViewCountIncreasable(projectId, currentTimeSeconds);
+    }
+
+    public Cookie createOrUpdateCookie(long projectId, ProjectViewTimes projectViewTimes) {
+        if (projectViewTimes.isEmpty()) {
+            return createCookie(projectId, projectViewTimes);
+        }
+        return updateCookie(projectId, projectViewTimes);
+    }
+
+    private Cookie createCookie(long projectId, ProjectViewTimes projectViewTimes) {
+        long currentTimeSeconds = getCurrentTimeSeconds();
+        long secondsUntilMidnight = getSecondsUntilMidnight(currentTimeSeconds);
+
+        projectViewTimes.add(projectId, currentTimeSeconds);
+        return buildValidCookie(projectViewTimes, secondsUntilMidnight);
+    }
+
+    private Cookie updateCookie(long projectId, ProjectViewTimes projectViewTimes) {
+        long currentTimeSeconds = getCurrentTimeSeconds();
+        long secondsUntilMidnight = getSecondsUntilMidnight(currentTimeSeconds);
+
+        projectViewTimes.removeExpiredEntries(currentTimeSeconds);
+        projectViewTimes.add(projectId, currentTimeSeconds);
+        projectViewTimes.removeUntilMaxSize();
+        return buildValidCookie(projectViewTimes, secondsUntilMidnight);
+    }
+
+    private Cookie buildValidCookie(ProjectViewTimes projectViewTimes, long secondsUntilMidnight) {
+        String json = projectViewTimes.serializeWithSizeLimit();
+        if (projectViewTimes.isInvalidForCookie(json)) {
+            return createEmptyCookie();
+        }
+        return buildCookieFromJson(json, secondsUntilMidnight);
+    }
+
+    private Cookie buildCookieFromJson(String json, long secondsUntilMidnight) {
+        String encoded = URLEncoder.encode(json, StandardCharsets.UTF_8);
+        Cookie cookie = new Cookie(COOKIE_NAME, encoded);
+        cookie.setPath(COOKIE_PATH);
+        cookie.setMaxAge((int) secondsUntilMidnight);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        return cookie;
+    }
+
+    private Cookie createEmptyCookie() {
+        Cookie cookie = new Cookie(COOKIE_NAME, "");
+        cookie.setPath(COOKIE_PATH);
+        cookie.setMaxAge(0);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        return cookie;
+    }
+
+    private long getCurrentTimeSeconds() {
+        return System.currentTimeMillis() / 1000;
+    }
+
+    private long getSecondsUntilMidnight(long currentTimeSeconds) {
+        ZonedDateTime nowKST = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
+        ZonedDateTime tomorrowMidnight = nowKST.plusDays(1).truncatedTo(ChronoUnit.DAYS);
+        long koreaMidnightTimestamp = tomorrowMidnight.toEpochSecond();
+        return koreaMidnightTimestamp - currentTimeSeconds;
+    }
+
+    private Cookie getCookie(HttpServletRequest request) {
+        if (request.getCookies() == null) {
+            return null;
+        }
+        return Arrays.stream(request.getCookies()).
+                filter(cookie -> cookie.getName().equals(COOKIE_NAME))
+                .findFirst()
+                .orElse(null);
+    }
+}
--- backend/src/main/java/moaon/backend/project/service/ProjectService.java
+++ backend/src/main/java/moaon/backend/project/service/ProjectService.java
@@ -2,6 +2,7 @@ package moaon.backend.project.service;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import moaon.backend.global.exception.custom.CustomException;
 import moaon.backend.global.exception.custom.ErrorCode;
 import moaon.backend.project.domain.Project;
@@ -13,18 +14,16 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ProjectService {
 
     private final ProjectRepository projectRepository;
 
-    @Transactional
     public ProjectDetailResponse getById(Long id) {
         Project project = projectRepository.findById(id)
                 .orElseThrow(() -> new CustomException(ErrorCode.PROJECT_NOT_FOUND));
-
-        project.addViewCount();
 
         return ProjectDetailResponse.from(project);
     }
@@ -33,5 +32,14 @@ public class ProjectService {
         List<Project> projects = projectRepository.findWithSearchConditions(projectQueryCondition);
 
         return ProjectSummaryResponse.from(projects);
+    }
+
+    @Transactional
+    public ProjectDetailResponse increaseViewsCount(long id) {
+        Project project = projectRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ErrorCode.PROJECT_NOT_FOUND));
+        project.addViewCount();
+
+        return ProjectDetailResponse.from(project);
     }
 }
