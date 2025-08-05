95c0afbd61be909a83287ffd75ff5d5afd1c8ec1
(minjae8563) <(minjae8563@naver.com)>
2025-08-05 15:37:31 +0900

    feat: PROJECT_NOT_FOUND 에러 코드 추가

--- backend/src/main/java/moaon/backend/global/exception/custom/ErrorCode.java
+++ backend/src/main/java/moaon/backend/global/exception/custom/ErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     UNKNOWN("UNKNOWN-001", "예기치 못한 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
-    PROJECT_NOT_FOUND("PROJECT-001", "프로젝트를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+    PROJECT_NOT_FOUND("PROJECT-001", "프로젝트를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    MISSING_REQUIRED_PARAMETER("GLOBAL-003", "필수 요청 파라미터가 누락되었습니다.", HttpStatus.BAD_REQUEST);
 
     private final String id;
     private final String message;
