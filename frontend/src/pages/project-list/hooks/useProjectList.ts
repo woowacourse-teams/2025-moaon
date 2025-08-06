4d1ed96ade294f4936cac61f35b8ac0ec4ed0faf
jin123457 <jinsol9909@gmail.com>
2025-08-06 20:44:14 +0900

    fix: 불필요한 console.log 삭제

--- frontend/src/pages/project-list/hooks/useProjectList.ts
+++ frontend/src/pages/project-list/hooks/useProjectList.ts
@@ -13,8 +13,6 @@ const useProjectList = () => {
     isRefetching,
   } = useInfiniteQuery(projectQueries.fetchList());
 
-  console.log(data);
-
   const projects = data?.pages.flatMap((page) => page.contents);
 
   // 첫 번째 페이지의 totalCount 사용
