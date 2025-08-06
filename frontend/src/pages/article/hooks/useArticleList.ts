750a8fbc83e629110613dfe03fb24ab00ae3dde0
jin123457 <jinsol9909@gmail.com>
2025-08-06 20:44:51 +0900

    refactor: useInfiniteQuery로 변경하여 무한스크롤 기능 추가

--- frontend/src/pages/article/hooks/useArticleList.ts
+++ frontend/src/pages/article/hooks/useArticleList.ts
@@ -1,14 +1,45 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQueryClient } from "@tanstack/react-query";
 import { articlesQueries } from "@/apis/articles/articles.queries";
 
 const useArticleList = () => {
+  const queryClient = useQueryClient();
   const {
-    data: articles,
+    data,
     isLoading,
-    refetch,
-  } = useQuery(articlesQueries.fetchList());
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    refetch: originalRefetch,
+    isRefetching,
+  } = useInfiniteQuery(articlesQueries.fetchList());
 
-  return { articles, isLoading, refetch };
+  const articles = data?.pages.flatMap((page) => page.articleContents);
+
+  const totalCount = data?.pages[0]?.totalCount;
+
+  const hasNext = data?.pages[data.pages.length - 1]?.hasNext ?? false;
+  const nextCursor = data?.pages[data.pages.length - 1]?.nextCursor ?? "";
+
+  const refetch = async () => {
+    await queryClient.resetQueries({
+      queryKey: articlesQueries.all,
+    });
+
+    return originalRefetch();
+  };
+
+  return {
+    articles,
+    totalCount,
+    hasNext,
+    nextCursor,
+    isLoading,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    refetch,
+    isRefetching,
+  };
 };
 
 export default useArticleList;
