08608ea8ea64de46ebdf63dd8fc4f0dd8ea5a7f9
eueo8259 <eueo8259@gmail.com>
2025-08-07 19:49:20 +0900

    refactor: 쿠키 경로 변경

--- backend/src/main/java/moaon/backend/global/config/CookieConfig.java
+++ backend/src/main/java/moaon/backend/global/config/CookieConfig.java
@@ -8,17 +8,16 @@ import org.springframework.context.annotation.Configuration;
 public class CookieConfig {
 
     private static final String PROJECT_COOKIE_NAME = "viewed_projects";
-    private static final String PROJECT_COOKIE_PATH = "/project";
+    private static final String COOKIE_PATH = "/";
     private static final String ARTICLE_COOKIE_NAME = "clicked_articles";
-    private static final String ARTICLE_COOKIE_PATH = "/article";
 
     @Bean
     public TrackingCookieManager projectViewCookieManager() {
-        return new TrackingCookieManager(PROJECT_COOKIE_NAME, PROJECT_COOKIE_PATH);
+        return new TrackingCookieManager(PROJECT_COOKIE_NAME, COOKIE_PATH);
     }
 
     @Bean
     public TrackingCookieManager articleClickCookieManager() {
-        return new TrackingCookieManager(ARTICLE_COOKIE_NAME, ARTICLE_COOKIE_PATH);
+        return new TrackingCookieManager(ARTICLE_COOKIE_NAME, COOKIE_PATH);
     }
 }
