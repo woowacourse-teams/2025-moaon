8d9bdc3a3fa34f2d3930cd395e49f867c83f5d08
mlnwns <mlnwns@naver.com>
2025-08-07 13:55:19 +0900

    feat: 프로젝트 상세페이지 아티클 관련 API 및 타입 정의 추가

---
+++ frontend/src/apis/projectArticles/getProjectArticles.ts
@@ -0,0 +1,13 @@
+import { httpClient } from "../HttpClient";
+import type { ProjectArticle } from "./projectArticles.type";
+
+const getProjectArticles = async (id: number): Promise<ProjectArticle> => {
+  const projectArticles = await httpClient.get(`/projects/${id}/articles`);
+  if (!projectArticles.ok) {
+    throw new Error("Failed to fetch project's articles");
+  }
+
+  return projectArticles.json();
+};
+
+export default getProjectArticles;
