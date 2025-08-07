fb65d8e1e6d08c625103d23c3f1b7da379a09161
jin123457 <jinsol9909@gmail.com>
2025-08-07 21:42:23 +0900

    feat: 아티클 클릭 수 기록 기능 추가

--- frontend/src/pages/article/ArticleBox/CardList/Card/Card.tsx
+++ frontend/src/pages/article/ArticleBox/CardList/Card/Card.tsx
@@ -1,5 +1,6 @@
 import { ARTICLE_CATEGORY_MAP } from "@domains/filter/articleCategory";
 import type { Article } from "@/apis/articles/articles.type";
+import useArticleList from "@/pages/article/hooks/useArticleList";
 import TechStackList from "@/pages/project-list/CardList/Card/TechStackList/TechStackList";
 import Badge from "./Badge/Badge";
 import * as S from "./Card.styled";
@@ -9,16 +10,24 @@ interface CardProps {
 }
 
 function Card({ article }: CardProps) {
+  const { postArticleClick } = useArticleList();
   const { title, summary, techStacks, url, category, projectId } = article;
   const { label, bgColor } = ARTICLE_CATEGORY_MAP[category];
+
   return (
     <S.CardContainer>
+      {article.clicks}
       <Badge text={label} bgColor={bgColor} />
       <S.CardTitle>{title}</S.CardTitle>
       <S.CardSummary>{summary}</S.CardSummary>
       <TechStackList techStacks={techStacks} />
       <S.BackDropBox className="back-drop-box">
-        <S.ArticleLink href={url} target="_blank" rel="noopener noreferrer">
+        <S.ArticleLink
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={() => postArticleClick(article.id)}
+        >
           아티클 보러가기
         </S.ArticleLink>
         <S.ProjectLink to={`/project/${projectId}`}>
