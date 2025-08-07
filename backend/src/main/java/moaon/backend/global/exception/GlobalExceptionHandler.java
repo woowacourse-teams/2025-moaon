d7e0ae28bd1aca611a81461f3f177675605bfce4
Minuring <mw9505@gmail.com>
2025-08-07 19:48:40 +0900

    feat: spring mvc 표준 예외 핸들링

--- backend/src/main/java/moaon/backend/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/moaon/backend/global/exception/GlobalExceptionHandler.java
@@ -4,7 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import moaon.backend.global.exception.custom.CustomException;
 import moaon.backend.global.exception.custom.ErrorCode;
 import moaon.backend.global.exception.dto.ErrorResponse;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
@@ -30,13 +35,44 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoResourceFoundException.class)
     public ResponseEntity<ErrorResponse> handleNoResourceFoundException(NoResourceFoundException e) {
-        ErrorCode errorCode = ErrorCode.RESOURCE_NOT_FOUND;
+        return handleMvcStandardException(ErrorCode.RESOURCE_NOT_FOUND, e.getMessage());
+    }
 
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ErrorResponse> handleMissingServletRequestParameterException(
+            MissingServletRequestParameterException e
+    ) {
+        return handleMvcStandardException(ErrorCode.MISSING_REQUIRED_PARAMETER, e.getMessage());
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(
+            HttpRequestMethodNotSupportedException e
+    ) {
+        return handleMvcStandardException(ErrorCode.HTTP_METHOD_NOT_SUPPORTED, e.getMessage());
+    }
+
+    @ExceptionHandler(TypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleTypeMismatchException(TypeMismatchException e) {
+        return handleMvcStandardException(ErrorCode.TYPE_MISMATCH, e.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return handleMvcStandardException(ErrorCode.HTTP_MESSAGE_NOT_READABLE, e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return handleMvcStandardException(ErrorCode.ARGUMENT_NOT_VALID, e.getMessage());
+    }
+
+    private ResponseEntity<ErrorResponse> handleMvcStandardException(ErrorCode errorCode, String exceptionMessage) {
         log.warn("[{}] {} {} | Detail: {}",
                 errorCode.name(),
                 errorCode.getId(),
                 errorCode.getMessage(),
-                e.getMessage()
+                exceptionMessage
         );
 
         return ResponseEntity
--- backend/src/main/java/moaon/backend/global/exception/custom/ErrorCode.java
+++ backend/src/main/java/moaon/backend/global/exception/custom/ErrorCode.java
@@ -9,8 +9,15 @@ public enum ErrorCode {
     UNKNOWN("GLOBAL-001", "예기치 못한 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     RESOURCE_NOT_FOUND("GLOBAL-002", "요청한 리소스를 찾을 수 없습니다", HttpStatus.NOT_FOUND),
     MISSING_REQUIRED_PARAMETER("GLOBAL-003", "필수 요청 파라미터가 누락되었습니다.", HttpStatus.BAD_REQUEST),
+    HTTP_METHOD_NOT_SUPPORTED("GLOBAL-004", "지원하지 않는 HTTP 메소드입니다.", HttpStatus.BAD_REQUEST),
+    TYPE_MISMATCH("GLOBAL-005", "요청 데이터의 자료형이 일치하지 않습니다. 경로 변수, 쿼리 파라미터, 요청 본문을 확인해 주세요.", HttpStatus.BAD_REQUEST),
+    HTTP_MESSAGE_NOT_READABLE("GLOBAL-006", "요청 본문을 파싱하지 못했습니다. 형식을 확인해 주세요.", HttpStatus.BAD_REQUEST),
+    ARGUMENT_NOT_VALID("GLOBAL-007", "요청 데이터에 대한 검증에 실패했습니다.", HttpStatus.BAD_REQUEST),
+
     PROJECT_NOT_FOUND("PROJECT-001", "프로젝트를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+
     ARTICLE_NOT_FOUND("ARTICLE-001", "아티클을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+
     INVALID_CURSOR_FORMAT("CURSOR-001", "커서 파라미터 형식이 올바르지 않습니다.", HttpStatus.BAD_REQUEST);
 
     private final String id;
