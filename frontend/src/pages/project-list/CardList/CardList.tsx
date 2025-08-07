aeaae193e202edddc0f9bfb8f02b697fa781ed70
eunoia-jaxson <eunoia@handong.ac.kr>
2025-08-07 14:26:56 +0900

    refactor: 무한스크롤 조건 변수 캡슐화

--- frontend/src/pages/project-list/CardList/CardList.tsx
+++ frontend/src/pages/project-list/CardList/CardList.tsx
@@ -1,26 +1,19 @@
-import { useMemo } from "react";
 import useInfiniteScroll from "@/shared/hooks/useInfiniteScroll";
 import useProjectList from "../hooks/useProjectList";
 import Card from "./Card/Card";
-import CardSkeleton from "./Card/CardSkeleton";
 import * as S from "./CardList.styled";
-
-const SKELETON_COUNT = 20;
+import CardSkeletonList from "./CardSkeletonList/CardSkeletonList";
 
 function CardList() {
   const {
     projects,
-    isLoading,
     hasNext,
     nextCursor,
     totalCount,
     fetchNextPage,
-    isFetchingNextPage,
-    isRefetching,
+    scrollEnabled,
+    showSkeleton,
   } = useProjectList();
-
-  const scrollEnabled = !isLoading && hasNext && !isFetchingNextPage;
-  const showSkeleton = isLoading || isFetchingNextPage || isRefetching;
 
   const { targetRef } = useInfiniteScroll({
     hasNext,
@@ -29,12 +22,6 @@ function CardList() {
     scrollEnabled,
   });
 
-  // 스켈레톤 키 생성 (메모이제이션으로 불필요한 재생성 방지)
-  const skeletonKeys = useMemo(
-    () => Array.from({ length: SKELETON_COUNT }, () => crypto.randomUUID()),
-    [],
-  );
-
   return (
     <section aria-label="프로젝트 목록">
       <S.ProjectIntro>
@@ -42,14 +29,12 @@ function CardList() {
         프로젝트가 모여있어요.
       </S.ProjectIntro>
       <S.CardList>
-        {/* 기존 프로젝트 카드들 */}
         {projects?.map((project) => (
           <Card key={project.id} project={project} />
         ))}
 
-        {showSkeleton && skeletonKeys.map((key) => <CardSkeleton key={key} />)}
+        {showSkeleton && <CardSkeletonList />}
 
-        {/* 무한스크롤 트리거 요소 */}
         {scrollEnabled && <div ref={targetRef} />}
       </S.CardList>
     </section>
