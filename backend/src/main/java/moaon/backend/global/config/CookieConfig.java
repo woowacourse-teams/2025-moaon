a5604255030e587743b63ab4343e69729509a9b9
eueo8259 <eueo8259@gmail.com>
2025-08-08 12:19:05 +0900

    fix : COOKIE_PATH 값 공백 삭제

--- backend/src/main/java/moaon/backend/global/config/CookieConfig.java
+++ backend/src/main/java/moaon/backend/global/config/CookieConfig.java
@@ -8,9 +8,9 @@ import org.springframework.context.annotation.Configuration;
 public class CookieConfig {
 
     private static final String PROJECT_COOKIE_NAME = "viewed_projects";
-    private static final String PROJECT_COOKIE_PATH = " /projects";
+    private static final String PROJECT_COOKIE_PATH = "/projects";
     private static final String ARTICLE_COOKIE_NAME = "clicked_articles";
-    private static final String ARTICLE_COOKIE_PATH = " /articles";
+    private static final String ARTICLE_COOKIE_PATH = "/articles";
 
     @Bean
     public TrackingCookieManager projectViewCookieManager() {
