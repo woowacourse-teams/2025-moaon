ae1ae84dd788e34ee96da9dcd0f033051269efdf
yesjuhee <juhee0924@g.skku.edu>
2025-08-10 14:04:37 +0900

    test: @Profile 값을 test로 변경 및 테스트 환경 yml 구성 추가
    - ErrorCodeController의 @Profile("local") -> @Profile("test")로 변경
    - 테스트 환경에서 사용될 application-test.yml 파일 추가

--- backend/src/test/java/moaon/backend/restdocs/ErrorCodeController.java
+++ backend/src/test/java/moaon/backend/restdocs/ErrorCodeController.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Profile("local")
+@Profile("test")
 @RestController
 public class ErrorCodeController {
 
