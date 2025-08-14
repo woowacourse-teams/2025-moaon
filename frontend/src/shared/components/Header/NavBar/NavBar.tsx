77e7d267601b25084e0cb96a60bcc4ad4bd293da
우혁 <woogur29@gmail.com>
2025-08-14 15:51:51 +0900

    refactor(FE): GNB UX 개선 (#270)
    * refactor: 헤더 링크 클릭 영역 확장
    
    * refactor: NavBar 컴포넌트에서 NavLink를 버튼으로 변경 및 네비게이션 로직 추가
    
    * refactor: SearchBar의 초기값 설정 로직 수정

--- frontend/src/shared/components/Header/NavBar/NavBar.tsx
+++ frontend/src/shared/components/Header/NavBar/NavBar.tsx
@@ -1,5 +1,7 @@
 import { useLayoutEffect, useRef, useState } from "react";
-import { useLocation } from "react-router";
+import { useLocation, useNavigate } from "react-router";
+import useArticleList from "@/pages/article/hooks/useArticleList";
+import useProjectList from "@/pages/project-list/hooks/useProjectList";
 import * as S from "./NavBar.styled";
 
 const NAV_LIST = [
@@ -20,6 +22,9 @@ function NavBar() {
   const linkRefs = useRef<(HTMLLIElement | null)[]>([]);
   const [underlineStyle, setUnderlineStyle] = useState({ left: 0, width: 0 });
   const activeIndex = NAV_LIST.findIndex((item) => item.href === pathname);
+  const navigate = useNavigate();
+  const { refetch: projectListRefetch } = useProjectList();
+  const { refetch: articleListRefetch } = useArticleList();
 
   useLayoutEffect(() => {
     const activeRef = linkRefs.current[activeIndex];
@@ -30,6 +35,20 @@ function NavBar() {
   }, [activeIndex]);
 
   const hasActive = activeIndex > -1;
+
+  const handleNavigation = (href: string) => {
+    navigate(href);
+
+    switch (href) {
+      case "/article":
+        articleListRefetch();
+        break;
+      case "/project":
+        projectListRefetch();
+        break;
+    }
+  };
+
   return (
     <S.NavBar>
       <S.NavLinkList>
@@ -44,7 +63,11 @@ function NavBar() {
               };
             }}
           >
-            <S.Link isSelected={activeIndex === idx} to={href} reloadDocument>
+            <S.Link
+              type="button"
+              isSelected={activeIndex === idx}
+              onClick={() => handleNavigation(href)}
+            >
               {text}
             </S.Link>
           </S.NavLinkItem>
