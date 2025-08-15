0b33bc571a7360af5886601b23371dbfd839dbea
wo-o29 <woogur29@gmail.com>
2025-08-15 23:19:25 +0900

    feat: 탭 컴포넌트 애니메이션 추가

--- frontend/src/shared/components/Tab/Tab.tsx
+++ frontend/src/shared/components/Tab/Tab.tsx
@@ -1,3 +1,4 @@
+import { useLayoutEffect, useRef, useState } from "react";
 import * as S from "./Tab.styled";
 
 interface TabItem<K extends string> {
@@ -19,17 +20,46 @@ function Tab<K extends string>({
   onSelect,
   width,
 }: TabProps<K>) {
+  const tabRefs = useRef<(HTMLLIElement | null)[]>([]);
+  const [selectedStyle, setSelectedStyle] = useState({
+    translateX: 0,
+    width: 0,
+  });
+  const selectedIndex = items.findIndex(({ key }) => key === selected);
+
+  useLayoutEffect(() => {
+    const active = tabRefs.current[selectedIndex];
+    if (active) {
+      const { offsetLeft, clientWidth } = active;
+      setSelectedStyle({ translateX: offsetLeft, width: clientWidth });
+    }
+  }, [selectedIndex]);
+
+  const animationDuration = 0.075 * items.length;
   return (
     <S.TabContainer width={width}>
       <S.TabItemList>
-        {items.map(({ key, label }) => {
+        <S.SlidingBG
+          translateX={selectedStyle.translateX}
+          width={selectedStyle.width}
+          duration={animationDuration}
+        />
+        {items.map(({ key, label }, idx) => {
           const isSelected = selected === key;
           return (
             <S.TabItem
+              ref={(el) => {
+                tabRefs.current[idx] = el;
+
+                return () => {
+                  tabRefs.current[idx] = null;
+                };
+              }}
               key={key}
               isSelected={isSelected}
               onClick={() => onSelect(key)}
               width={width ? width / items.length : undefined}
+              duration={animationDuration}
             >
               {label}
             </S.TabItem>
