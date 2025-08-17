99e6bea8e39c130f447d5426d88b4b49b04f1e57
우혁 <woogur29@gmail.com>
2025-08-17 20:11:01 +0900

    feat(FE): 탭 컴포넌트 애니메이션 추가 (#284)
    * refactor: NavBar translateX 변경 방식으로 수정
    
    * feat: 탭 컴포넌트 애니메이션 추가
    
    * feat: useTabAnimation 커스텀 훅 추가
    
    * refactor: useTabAnimation 훅 적용
    
    * feat: useIsMounted 커스텀 훅 추가
    
    * refactor: 첫 마운트 시점에는 애니메이션 동작하지 않도록 수정
    
    * refactor: duration 수정

--- frontend/src/shared/components/Tab/Tab.tsx
+++ frontend/src/shared/components/Tab/Tab.tsx
@@ -1,3 +1,4 @@
+import { useTabAnimation } from "@shared/hooks/useTabAnimation";
 import * as S from "./Tab.styled";
 
 interface TabItem<K extends string> {
@@ -19,17 +20,30 @@ function Tab<K extends string>({
   onSelect,
   width,
 }: TabProps<K>) {
+  const selectedIndex = items.findIndex(({ key }) => key === selected);
+  const { setTabElementsRef, selectedStyle } = useTabAnimation({
+    selectedIndex,
+    duration: 0.075 * items.length,
+  });
+
   return (
     <S.TabContainer width={width}>
       <S.TabItemList>
-        {items.map(({ key, label }) => {
+        <S.SlidingBG
+          translateX={selectedStyle.translateX}
+          width={selectedStyle.width}
+          duration={selectedStyle.duration}
+        />
+        {items.map(({ key, label }, idx) => {
           const isSelected = selected === key;
           return (
             <S.TabItem
+              ref={(el) => setTabElementsRef(el, idx)}
               key={key}
               isSelected={isSelected}
               onClick={() => onSelect(key)}
               width={width ? width / items.length : undefined}
+              duration={selectedStyle.duration}
             >
               {label}
             </S.TabItem>
