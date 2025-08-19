f4fdd9797637a4cd8b9fc60d6406407f4c4363ec
eunoia-jaxson <eunoia@handong.ac.kr>
2025-08-19 16:13:17 +0900

    feat(FE): 웹팩 설정에서 Sentry 플러그인 임포트 위치 수정

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,6 +1,6 @@
+import { sentryWebpackPlugin } from "@sentry/webpack-plugin";
 import { merge } from "webpack-merge";
 import common from "./webpack.common.js";
-import { sentryWebpackPlugin } from "@sentry/webpack-plugin";
 
 export default merge(common, {
   mode: "production",
