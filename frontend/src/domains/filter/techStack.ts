4e566fae4a3d91b5c75df8ddc2dbd644ca7f14e0
jin123457 <jinsol9909@gmail.com>
2025-08-21 17:22:58 +0900

    feat: RecyclerView 아이콘 추가

--- frontend/src/domains/filter/techStack.ts
+++ frontend/src/domains/filter/techStack.ts
@@ -535,6 +535,10 @@ export const ANDROID_STACK_ICON_MAP = {
     label: "React Native",
     imgUrl: "https://techcourse-project-2025.s3.ap-northeast-2.amazonaws.com/moaon/teckstack-icons/ReactNative.svg",
   },
+  recyclerView: {
+    label: "RecyclerView",
+    imgUrl: "https://techcourse-project-2025.s3.ap-northeast-2.amazonaws.com/moaon/teckstack-icons/Jetpack.svg",
+  },
 } as const;
 
 export const IOS_STACK_ICON_MAP = {
