5c24c6d7a55eb2990ffc5127037773f073731e3b
yesjuhee <juhee0924@g.skku.edu>
2025-09-17 19:01:18 +0900

    chore: build.gradle 파일 추가

--- backend/src/test/java/moaon/backend/db/DataGeneratorTest.java
+++ backend/src/test/java/moaon/backend/db/DataGeneratorTest.java
@@ -18,10 +18,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("local")
 @Disabled
 public class DataGeneratorTest {
 
