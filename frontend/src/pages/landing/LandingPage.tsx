144c0dd3db313f06fc2c554382483de574f76ad7
jin123457 <jinsol9909@gmail.com>
2025-09-23 15:18:02 +0900

    rename: whoSection => TargetUsersSection 폴더명 변경

--- frontend/src/pages/landing/LandingPage.tsx
+++ frontend/src/pages/landing/LandingPage.tsx
@@ -2,7 +2,7 @@ import MoveTop from "@shared/components/MoveTop/MoveTop";
 import FeatureSection from "./components/FeatureSection/FeatureSection";
 import HeroSection from "./components/HeroSection/HeroSection";
 import LandingBannerSection from "./components/LandingBannerSection/LandingBannerSection";
-import TargetUsersSection from "./components/WhoSection/TargetUsersSection";
+import TargetUsersSection from "./components/TargetUsersSection/TargetUsersSection";
 
 function LandingPage() {
   return (
