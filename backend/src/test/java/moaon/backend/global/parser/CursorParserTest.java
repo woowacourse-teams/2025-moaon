55305957dc9687d7a83d2dfe424aa479353851a2
minjae <minjae8563@naver.com>
2025-09-25 21:09:54 +0900

    feat: cursor 형식 수정 (#418)
    Co-authored-by: minjae8563 <(minjae8563@naver.com)>

---
+++ backend/src/test/java/moaon/backend/global/parser/CursorParserTest.java
@@ -0,0 +1,26 @@
+package moaon.backend.global.parser;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import moaon.backend.project.domain.ProjectSortType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CursorParserTest {
+
+    @DisplayName("cursor 형식을 검증한다.")
+    @Test
+    void cursorParse() {
+        // given
+        String cursor1 = "2025-09-25T04:35:00.764_35867";
+        String cursor2 = "2025-09-25T04:35:00_35867";
+        ProjectSortType createdAt = ProjectSortType.CREATED_AT;
+
+        // when - then
+        assertAll(
+                () -> assertDoesNotThrow(() -> createdAt.toCursor(cursor2)),
+                () -> assertDoesNotThrow(() -> createdAt.toCursor(cursor2))
+        );
+    }
+}
