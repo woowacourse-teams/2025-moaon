95ca9fe8780c06dcced538086287b514ee917aa7
(minjae8563) <(minjae8563@naver.com)>
2025-09-25 21:26:45 +0900

    refactor: cursor 형식 수정

--- backend/src/test/java/moaon/backend/global/parser/CursorParserTest.java
+++ backend/src/test/java/moaon/backend/global/parser/CursorParserTest.java
@@ -15,12 +15,14 @@ public class CursorParserTest {
         // given
         String cursor1 = "2025-09-25T04:35:00.764_35867";
         String cursor2 = "2025-09-25T04:35:00_35867";
+        String cursor3 = "2025-09-25T12:13:36.146232141_35867";
         ProjectSortType createdAt = ProjectSortType.CREATED_AT;
 
         // when - then
         assertAll(
+                () -> assertDoesNotThrow(() -> createdAt.toCursor(cursor1)),
                 () -> assertDoesNotThrow(() -> createdAt.toCursor(cursor2)),
-                () -> assertDoesNotThrow(() -> createdAt.toCursor(cursor2))
+                () -> assertDoesNotThrow(() -> createdAt.toCursor(cursor3))
         );
     }
 }
