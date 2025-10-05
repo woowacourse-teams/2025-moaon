f6b43815814ef9b8b196bb0cc7e6477a38242999
wo-o29 <woogur29@gmail.com>
2025-10-05 16:17:07 +0900

    feat: EventProjectList 컴포넌트 추가

---
+++ frontend/src/pages/wooteco/company/EventProjectList/EventProjectList.styled.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const List = styled.ul`
+  margin: 1rem auto;
+  width: 80rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(18.75rem, 1fr));
+  gap: 1.5rem;
+`;
