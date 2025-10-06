962a8fafcd10d3fd1a27e3cc479b03840dfabb81
wo-o29 <woogur29@gmail.com>
2025-10-06 21:52:52 +0900

    style: 반응형 스타일 추가

--- frontend/src/pages/wooteco/company/EventProjectList/EventProjectList.styled.ts
+++ frontend/src/pages/wooteco/company/EventProjectList/EventProjectList.styled.ts
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { BP_1280 } from "@/styles/global.styled";
 
 export const List = styled.ul`
   margin: 1rem auto;
@@ -6,4 +7,8 @@ export const List = styled.ul`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(18.75rem, 1fr));
   gap: 1.5rem;
+
+  ${BP_1280} {
+    width: 100%;
+  }
 `;
