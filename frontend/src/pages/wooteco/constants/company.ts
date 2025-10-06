f2da9fa49c16476e7ae33ee78389a0e1d8855d40
wo-o29 <woogur29@gmail.com>
2025-10-06 21:52:38 +0900

    refactor: 기업 리스트 순서 변경

--- frontend/src/pages/wooteco/constants/company.ts
+++ frontend/src/pages/wooteco/constants/company.ts
@@ -1,19 +1,19 @@
 export const COMPANY_LIST = [
-  "woowa",
   "naver",
   "kakao",
   "line",
   "toss",
   "dangn",
+  "woowa",
 ] as const;
 
 export type Company = (typeof COMPANY_LIST)[number];
 
 export const COMPANY_NAME: Record<Company, string> = {
-  woowa: "우아한형제들",
   naver: "네이버",
   toss: "토스",
   kakao: "카카오",
   line: "라인",
   dangn: "당근마켓",
+  woowa: "우아한형제들",
 };
