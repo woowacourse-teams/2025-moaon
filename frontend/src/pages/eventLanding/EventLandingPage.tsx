6716e9dae3e5b64ca6bceb44fca8a7490b73c7b6
wo-o29 <woogur29@gmail.com>
2025-10-09 12:42:29 +0900

    feat: EventLandingPage 컴포넌트 추가

---
+++ frontend/src/pages/eventLanding/EventLandingPage.tsx
@@ -0,0 +1,22 @@
+import FullPageSlide from "@shared/components/FullPageSlide/FullPageSlide";
+import Event1 from "./Event1/Event1";
+import Event2 from "./Event2/Event2";
+import EventSection from "./EventSection/EventSection";
+
+const pages = [
+  { id: 1, color: "#FF6B6B", title: "Event 1", render: () => <Event1 /> },
+  { id: 2, color: "#00A8FF", title: "Event 2", render: () => <Event2 /> },
+  { id: 3, color: "#45B7D1", title: "Event 3", render: () => <Event1 /> },
+];
+
+export default function EventLandingPage() {
+  return (
+    <FullPageSlide totalIndex={pages.length - 1} excludedHeight={76}>
+      {pages.map(({ id, color, title, render }) => (
+        <EventSection key={id} badgeColor={color} badgeText={title}>
+          {render()}
+        </EventSection>
+      ))}
+    </FullPageSlide>
+  );
+}
