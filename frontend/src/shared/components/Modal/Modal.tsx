da2f02ec373329705db4d592704dd247e597943d
mlnwns <mlnwns@naver.com>
2025-10-10 17:41:29 +0900

    refactor: Modal 컴포넌트에 웹 접근성 부여

--- frontend/src/shared/components/Modal/Modal.tsx
+++ frontend/src/shared/components/Modal/Modal.tsx
@@ -1,7 +1,7 @@
 import { useFocusTrap } from "@shared/hooks/useFocusTrap";
 import { useKeyDown } from "@shared/hooks/useKeyDown/useKeyDown";
 import { useOutsideClick } from "@shared/hooks/useOutsideClick";
-import { type PropsWithChildren, useRef } from "react";
+import { type PropsWithChildren, useId, useRef } from "react";
 import ReactDOM from "react-dom";
 import { usePreventScroll } from "./hooks/usePreventScroll";
 import * as S from "./Modal.styled";
@@ -9,9 +9,17 @@ import * as S from "./Modal.styled";
 interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
+  titleId?: string;
+  descriptionId?: string;
 }
 
-function Modal({ isOpen, onClose, children }: PropsWithChildren<ModalProps>) {
+function Modal({
+  isOpen,
+  onClose,
+  children,
+  titleId,
+  descriptionId,
+}: PropsWithChildren<ModalProps>) {
   useKeyDown({ Escape: onClose });
   usePreventScroll(isOpen);
 
@@ -20,15 +28,22 @@ function Modal({ isOpen, onClose, children }: PropsWithChildren<ModalProps>) {
 
   const setOutsideClickRef = useOutsideClick(onClose);
 
+  const generatedTitleId = useId();
+  const generatedDescriptionId = useId();
+
   if (!isOpen) return null;
 
   const modalContent = (
-    <S.Overlay>
+    <S.Overlay role="presentation">
       <S.Content
         ref={(el) => {
           contentRef.current = el;
           setOutsideClickRef(el);
         }}
+        role="dialog"
+        aria-modal="true"
+        aria-labelledby={titleId ?? generatedTitleId}
+        aria-describedby={descriptionId ?? generatedDescriptionId}
       >
         {children}
       </S.Content>
