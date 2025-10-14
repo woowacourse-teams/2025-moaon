af9d46f77159fc4091e6f5bf576e31cd5e2ba12b
mlnwns <mlnwns@naver.com>
2025-10-14 21:09:25 +0900

    feat: ProjectFormDataType 및 ArticleFormDataType 인터페이스 추가

---
+++ frontend/src/pages/register/types.ts
@@ -0,0 +1,23 @@
+import type { ArticleSectorKey } from "@domains/filter/articleSector";
+import type { AllTopicKey } from "@domains/filter/articleTopic";
+import type { ProjectCategoryKey } from "@domains/filter/projectCategory";
+import type { TechStackKey } from "@domains/filter/techStack";
+
+export interface ProjectFormDataType {
+  title: string;
+  description: string;
+  githubUrl: string;
+  serviceUrl: string;
+  topics: ProjectCategoryKey[];
+  techStacks: TechStackKey[];
+}
+
+export interface ArticleFormDataType {
+  id: string;
+  address: string;
+  title: string;
+  description: string;
+  sector: ArticleSectorKey;
+  topics: AllTopicKey[];
+  techStacks: TechStackKey[];
+}
