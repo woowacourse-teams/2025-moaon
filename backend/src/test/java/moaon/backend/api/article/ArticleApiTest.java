4fcb4f7fa91f672c5d08cd2623efe4ed9b41fb6c
(minjae8563) <(minjae8563@naver.com)>
2025-10-15 21:44:49 +0900

    test: 로그인 상황 코드 구현
    Co-authored-by: Minuring <mw9505@gmail.com>
    Co-authored-by: minjae <minjae8563@naver.com>

--- backend/src/test/java/moaon/backend/api/article/ArticleApiTest.java
+++ backend/src/test/java/moaon/backend/api/article/ArticleApiTest.java
@@ -27,10 +27,16 @@ import moaon.backend.fixture.Fixture;
 import moaon.backend.fixture.ProjectFixtureBuilder;
 import moaon.backend.fixture.RepositoryHelper;
 import moaon.backend.global.config.QueryDslConfig;
+import moaon.backend.member.domain.Member;
+import moaon.backend.member.repository.MemberRepository;
+import moaon.backend.member.service.JwtTokenProvider;
+import moaon.backend.member.service.OAuthService;
 import moaon.backend.project.domain.Project;
 import moaon.backend.techStack.domain.TechStack;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.restdocs.payload.RequestFieldsSnippet;
@@ -46,6 +52,25 @@ public class ArticleApiTest extends BaseApiTest {
 
     @MockitoBean
     private ArticleDocumentRepository documentRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @MockitoBean
+    private OAuthService oAuthService;
+
+    private String token;
+
+    @BeforeEach
+    void cookieSetUp() {
+        Member member = Fixture.anyMember();
+        repositoryHelper.save(member);
+
+        token = jwtTokenProvider.createToken(member.getSocialId());
+    }
 
     @DisplayName("POST /articles: 아티클 저장 API")
     @Test
@@ -70,9 +95,12 @@ public class ArticleApiTest extends BaseApiTest {
                 .topics(List.of("etc"))
                 .build();
 
+        Mockito.doNothing().when(oAuthService).validateToken(Mockito.any());
+
         // when
         RestAssured.given(documentationSpecification).log().all()
                 .contentType(ContentType.JSON)
+                .cookie("token", token)
                 .body(List.of(articleCreateRequest))
                 .filter(document(articleCreateRequestFields()))
                 .when().post("/articles")
