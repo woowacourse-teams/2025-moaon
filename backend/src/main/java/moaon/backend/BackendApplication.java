c1271f7f912a61fc018070b30e378455e5754c75
yesjuhee <juhee0924@g.skku.edu>
2025-10-15 19:43:15 +0900

    feat: 롤백 기능 구현 및 롤백 테스트용 에러 추가

--- backend/src/main/java/moaon/backend/BackendApplication.java
+++ backend/src/main/java/moaon/backend/BackendApplication.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class BackendApplication {
 
     public static void main(String[] args) {
+        int a = 1 / 0;
         SpringApplication.run(BackendApplication.class, args);
     }
 }
