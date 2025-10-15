f3292ede42877c1f28c887627f36265a43693d03
(minjae8563) <(minjae8563@naver.com)>
2025-10-15 19:16:06 +0900

    refactor: 에러메세지 수정
    Co-authored-by: Minuring <mw9505@gmail.com>
    Co-authored-by: minjae <minjae8563@naver.com>

--- backend/src/main/java/moaon/backend/article/dto/ArticleCrawlRequest.java
+++ backend/src/main/java/moaon/backend/article/dto/ArticleCrawlRequest.java
@@ -1,8 +1,9 @@
 package moaon.backend.article.dto;
 
+import jakarta.validation.constraints.NotNull;
 import java.net.URL;
 
 public record ArticleCrawlRequest(
-        URL url
+        @NotNull @org.hibernate.validator.constraints.URL URL url
 ) {
 }
