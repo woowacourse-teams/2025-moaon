9ca4e1557f8561eab76dea5fd1bb41b0272d3120
(minjae8563) <(minjae8563@naver.com)>
2025-10-15 19:06:41 +0900

    refactor: 변수명 변경
    Co-authored-by: Minuring <mw9505@gmail.com>
    Co-authored-by: minjae <minjae8563@naver.com>

--- backend/src/main/java/moaon/backend/global/exception/custom/CustomException.java
+++ backend/src/main/java/moaon/backend/global/exception/custom/CustomException.java
@@ -6,16 +6,16 @@ import lombok.Getter;
 public class CustomException extends RuntimeException {
 
     private final ErrorCode errorCode;
-    private Throwable throwable;
+    private Throwable cause;
 
     public CustomException(ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
 
-    public CustomException(ErrorCode errorCode, Throwable throwable) {
+    public CustomException(ErrorCode errorCode, Throwable cause) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
-        this.throwable = throwable;
+        this.cause = cause;
     }
 }
