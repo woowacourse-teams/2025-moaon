aad326dc2aa77c00c794d8084f6458ccedcb0e55
eunoia-jaxson <eunoia@handong.ac.kr>
2025-10-15 15:35:26 +0900

    style: Header 롤백

--- frontend/src/shared/components/Header/RegisterProjectButton/RegisterProjectButton.styled.ts
+++ frontend/src/shared/components/Header/RegisterProjectButton/RegisterProjectButton.styled.ts
@@ -16,27 +16,9 @@ export const RegisterProjectButton = styled.button`
     background-color: #0472e8ff;
     transition: background-color 0.3s ease;
   }
-  @media screen and (max-width: 1024px) {
-    font-size: 13px;
-    padding: 9px 13px 9px 15px;
-  }
-  @media screen and (max-width: 768px) {
-    font-size: 12px;
-    padding: 8px 12px 8px 14px;
-  }
-  @media screen and (max-width: 480px) {
-    font-size: 11.5px;
-    padding: 7px 11px 7px 13px;
-  }
 `;
 
 export const Icon = styled.img`
   width: 14px;
   aspect-ratio: 1/1;
-  @media screen and (max-width: 768px) {
-    width: 13px;
-  }
-  @media screen and (max-width: 480px) {
-    width: 12px;
-  }
 `;
