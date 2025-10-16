8b7f14856dac5faa9a4aa96ef1abedb5eefee3e9
wo-o29 <woogur29@gmail.com>
2025-10-16 22:20:06 +0900

    style: 색상 변경

--- frontend/src/pages/eventLanding/EventLandingPage.tsx
+++ frontend/src/pages/eventLanding/EventLandingPage.tsx
@@ -6,7 +6,7 @@ import EventSection from "./EventSection/EventSection";
 
 const pages = [
   { id: 1, color: "#FF6B6B", title: "Event 1", render: () => <Event1 /> },
-  { id: 2, color: "#00A8FF", title: "Event 2", render: () => <Event2 /> },
+  { id: 2, color: "#12b886", title: "Event 2", render: () => <Event2 /> },
   { id: 3, color: "#45B7D1", title: "Event 3", render: () => <Event3 /> },
 ];
 
