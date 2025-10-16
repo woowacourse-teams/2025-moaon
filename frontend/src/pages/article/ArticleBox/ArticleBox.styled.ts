629050eedcd37542dee3dd9106f369faf04f01ee
mlnwns <mlnwns@naver.com>
2025-10-16 20:49:38 +0900

    style: 반응형 디자인을 위한 ArticleBox 및 ArticlePage 스타일 조정

--- frontend/src/pages/article/ArticleBox/ArticleBox.styled.ts
+++ frontend/src/pages/article/ArticleBox/ArticleBox.styled.ts
@@ -5,4 +5,15 @@ export const ArticleBoxContainer = styled.div`
   flex-direction: column;
   gap: 1.25rem;
   width: 100%;
+
+  @media screen and (max-width: 1280px) {
+    padding: 0 1.5rem;
+  }
+
+  @media screen and (max-width: 1024px) {
+    padding: 0 1rem;
+  }
+  @media screen and (max-width: 480px) {
+    padding: 0 0.5rem;
+  }
 `;
