b9e72d7b304b1de7ec16265035f7fa89f94ce6f7
jin123457 <jinsol9909@gmail.com>
2025-10-17 15:15:03 +0900

    style: CardListContainer 반응형 스타일 추가

--- frontend/src/pages/article/ArticleBox/CardList/CardList.styled.ts
+++ frontend/src/pages/article/ArticleBox/CardList/CardList.styled.ts
@@ -1,10 +1,15 @@
 import styled from "@emotion/styled";
+import { BP_480 } from "@/styles/global.styled";
 
 export const CardListContainer = styled.ul`
   width: 100%;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(25rem, 1fr));
   gap: 1.375rem 2rem;
+
+  ${BP_480} {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `;
 
 export const EmptyContainer = styled.div`
