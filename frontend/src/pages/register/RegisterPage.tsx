1cbb82ace4c9fe412ad2cbe9c99657d64f733797
JIN <jinsol9909@gmail.com>
2025-10-17 14:34:43 +0900

    feat(FE): useWindowSize 공용 훅 개발 (#479)
    * refactor: 아티클 클릭 수 기록 기능 함수명 및 파일명 명시적으로 수정
    
    * feat: useWindowSize 공용 훅 추가하여 창 크기 변경 감지 기능 구현

---
+++ frontend/src/pages/register/RegisterPage.tsx
@@ -0,0 +1,7 @@
+import ArticleSubmission from "@domains/components/ArticleSubmission/ArticleSubmission";
+
+function RegisterPage() {
+  return <ArticleSubmission projectId={1} />;
+}
+
+export default RegisterPage;
