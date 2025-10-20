c61a5fa5db68ed88563427b4057bd452e2905266
(minjae8563) <(minjae8563@naver.com)>
2025-10-20 10:52:12 +0900

    feat: 로그인 후 리디렉션 하도록 로직 수정
    Co-authored-by: Minuring <mw9505@gmail.com>
    Co-authored-by: minjae <minjae8563@naver.com>

--- backend/src/main/java/moaon/backend/member/controller/GoogleLoginController.java
+++ backend/src/main/java/moaon/backend/member/controller/GoogleLoginController.java
@@ -2,10 +2,12 @@ package moaon.backend.member.controller;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import moaon.backend.global.exception.custom.CustomException;
+import moaon.backend.global.exception.custom.ErrorCode;
 import moaon.backend.member.dto.JwtToken;
 import moaon.backend.member.service.GoogleLoginService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,20 +21,25 @@ public class GoogleLoginController {
     private final GoogleLoginService googleLoginService;
 
     @GetMapping
-    public ResponseEntity<Void> login(
+    public void login(
             @RequestParam String code,
             HttpServletResponse response
     ) {
-        JwtToken token = googleLoginService.login(code);
-        Cookie cookie = new Cookie("token", token.token());
-        response.addCookie(new Cookie("token", token.token()));
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(60 * 60 * 24);
-        cookie.setAttribute("SameSite", "None");
+        try {
+            JwtToken token = googleLoginService.login(code);
+            Cookie cookie = new Cookie("token", token.token());
+            response.addCookie(new Cookie("token", token.token()));
+            cookie.setHttpOnly(true);
+            cookie.setSecure(true);
+            cookie.setPath("/");
 
-        response.addCookie(cookie);
-        return ResponseEntity.ok().build();
+            cookie.setMaxAge(60 * 60 * 24);
+            cookie.setAttribute("SameSite", "None");
+            response.addCookie(cookie);
+
+            response.sendRedirect("/oauth/callback");
+        } catch (IOException e) {
+            throw new CustomException(ErrorCode.UNKNOWN, e);
+        }
     }
 }
