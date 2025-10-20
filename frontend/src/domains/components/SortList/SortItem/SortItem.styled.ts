e16d704532f1153ae044c044566ba046d5e265a1
eunoia-jaxson <eunoia@handong.ac.kr>
2025-10-20 14:15:20 +0900

    style: SortList 및 SortItem 컴포넌트의 반응형 디자인 개선을 위한 미디어 쿼리 통합 및 속성 조정

--- frontend/src/domains/components/SortList/SortItem/SortItem.styled.ts
+++ frontend/src/domains/components/SortList/SortItem/SortItem.styled.ts
@@ -1,17 +1,19 @@
 import styled from "@emotion/styled";
+import { BP_480, BP_768, BP_1024 } from "@/styles/global.styled";
 
 export const Item = styled.li``;
 
 export const Button = styled.button<{ isSelected: boolean }>`
   color: ${({ isSelected }) => (isSelected ? "#000" : "#aaa")};
   font-size: 1.0625rem;
-  @media screen and (max-width: 1024px) {
+
+  ${BP_1024} {
     font-size: 1rem;
   }
-  @media screen and (max-width: 768px) {
+  ${BP_768} {
     font-size: 0.85rem;
   }
-  @media screen and (max-width: 480px) {
+  ${BP_480} {
     font-size: 0.9rem;
   }
 `;
