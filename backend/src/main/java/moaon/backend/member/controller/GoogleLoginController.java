33698b2251240608b74224261b9a7850f6529daf
Minuring <144205824+Minuring@users.noreply.github.com>
2025-10-21 22:36:04 +0900

    refactor(BE): redirectBase를 파라미터로 받아 리다이렉트하도록 변경 (#526)
    * refactor: redirectUri를 바디로 응답하도록 변경
    
    Co-authored-by: minjae <minjae8563@naver.com>
    
    * refactor: redirectBase를 파라미터로 받도록 변경
    
    Co-authored-by: minjae <minjae8563@naver.com>
    
    * refactor: code, state 필수로 변경
    
    Co-authored-by: minjae <minjae8563@naver.com>
    
    ---------
    
    Co-authored-by: minjae <minjae8563@naver.com>

--- backend/src/main/java/moaon/backend/member/controller/GoogleLoginController.java
+++ backend/src/main/java/moaon/backend/member/controller/GoogleLoginController.java
@@ -1,13 +1,21 @@
 package moaon.backend.member.controller;
 
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.json.JsonException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import moaon.backend.global.exception.custom.CustomException;
+import moaon.backend.global.exception.custom.ErrorCode;
 import moaon.backend.member.dto.JwtToken;
-import moaon.backend.member.dto.LoginResponse;
 import moaon.backend.member.service.GoogleLoginService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,29 +27,59 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class GoogleLoginController {
 
+    private static final String REDIRECT_BASE_KEY = "redirectBase";
+
     private final GoogleLoginService googleLoginService;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @GetMapping
-    public ResponseEntity<LoginResponse> login(
-            @RequestParam String code,
+    public void login(
+            @RequestParam(value = "code", required = true) String code,
+            @RequestParam(value = "state", required = true) String state,
             HttpServletResponse response
     ) {
+        String redirectBase = parseRedirectBase(state);
+
         try {
             JwtToken token = googleLoginService.login(code);
-            Cookie cookie = new Cookie("token", token.token());
-            response.addCookie(new Cookie("token", token.token()));
-            cookie.setHttpOnly(true);
-            cookie.setSecure(true);
-            cookie.setPath("/");
-
-            cookie.setMaxAge(60 * 60 * 24);
-            cookie.setAttribute("SameSite", "None");
+            Cookie cookie = createTokenCookie(token);
             response.addCookie(cookie);
+            response.sendRedirect(redirectBase + "/oauth/callback/success");
+            log.info("구글 로그인에 성공하여 리다이렉트 되었습니다. redirectBase: {}", redirectBase);
 
-            return ResponseEntity.ok(new LoginResponse("/oauth/callback/success"));
-        } catch (Exception e) {
-            log.error("구글 로그인에 실패했습니다.", e);
-            return ResponseEntity.ok(new LoginResponse("/oauth/callback/failed"));
+        } catch (Exception ex) {
+            try {
+                response.sendRedirect(redirectBase + "/oauth/callback/failed");
+                log.warn("구글 로그인에 실패하여 리다이렉트 되었습니다. redirectBase: {}", redirectBase, ex);
+
+            } catch (IOException e) {
+                log.error("구글 로그인 및 리디렉션에 실패했습니다.", e);
+                throw new CustomException(ErrorCode.UNKNOWN, e);
+            }
         }
+    }
+
+    private String parseRedirectBase(String state) {
+        try {
+            String urlDecodedState = URLDecoder.decode(state, StandardCharsets.UTF_8);
+            Map<?, ?> stateMap = objectMapper.readValue(urlDecodedState, Map.class);
+            return (String) Optional.ofNullable(stateMap.get(REDIRECT_BASE_KEY))
+                    .orElseThrow(() -> new CustomException(ErrorCode.LOGIN_REDIRECT_FAILED));
+
+        } catch (JsonException | JacksonException jsonEx) {
+            log.warn("구글 로그인에 실패했습니다. redirectBase를 파싱할 수 없습니다.", jsonEx);
+            throw new CustomException(ErrorCode.LOGIN_REDIRECT_FAILED, jsonEx);
+        }
+    }
+
+    private Cookie createTokenCookie(JwtToken token) {
+        Cookie cookie = new Cookie("token", token.token());
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+
+        cookie.setMaxAge(60 * 60 * 24);
+        cookie.setAttribute("SameSite", "None");
+        return cookie;
     }
 }
