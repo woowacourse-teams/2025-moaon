166d8fda7092848fcc844ee79434dd27fc976ef5
minjae <minjae8563@naver.com>
2025-10-21 15:15:19 +0900

    feat(BE): 로그인 후 리디렉션 하도록 로직 수정 (#502)
    * feat: 로그인 후 리디렉션 하도록 로직 수정
    
    Co-authored-by: Minuring <mw9505@gmail.com>
    Co-authored-by: minjae <minjae8563@naver.com>
    
    * refactor: 리디렉션 URL 성공 여부에 따라 구분
    
    Co-authored-by: minjae <minjae8563@naver.com>
    
    ---------
    
    Co-authored-by: (minjae8563) <(minjae8563@naver.com)>
    Co-authored-by: Minuring <mw9505@gmail.com>

--- backend/src/main/java/moaon/backend/member/controller/GoogleLoginController.java
+++ backend/src/main/java/moaon/backend/member/controller/GoogleLoginController.java
@@ -2,15 +2,19 @@ package moaon.backend.member.controller;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import moaon.backend.global.exception.custom.CustomException;
+import moaon.backend.global.exception.custom.ErrorCode;
 import moaon.backend.member.dto.JwtToken;
 import moaon.backend.member.service.GoogleLoginService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/login/oauth2/code/google")
 @RequiredArgsConstructor
@@ -19,20 +23,30 @@ public class GoogleLoginController {
     private final GoogleLoginService googleLoginService;
 
     @GetMapping
-    public ResponseEntity<Void> login(
+    public void login(
             @RequestParam String code,
             HttpServletResponse response
     ) {
-        JwtToken token = googleLoginService.login(code);
-        Cookie cookie = new Cookie("token", token.token());
-        response.addCookie(new Cookie("token", token.token()));
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(60 * 60 * 24);
-        cookie.setAttribute("SameSite", "None");
+        try {
+            JwtToken token = googleLoginService.login(code);
+            Cookie cookie = new Cookie("token", token.token());
+            response.addCookie(new Cookie("token", token.token()));
+            cookie.setHttpOnly(true);
+            cookie.setSecure(true);
+            cookie.setPath("/");
 
-        response.addCookie(cookie);
-        return ResponseEntity.ok().build();
+            cookie.setMaxAge(60 * 60 * 24);
+            cookie.setAttribute("SameSite", "None");
+            response.addCookie(cookie);
+
+            response.sendRedirect("/oauth/callback/success");
+        } catch (Exception e) {
+            try {
+                response.sendRedirect("/oauth/callback/failed");
+                log.error("구글 로그인에 실패했습니다.", e);
+            } catch (IOException ex) {
+                throw new CustomException(ErrorCode.UNKNOWN, e);
+            }
+        }
     }
 }
