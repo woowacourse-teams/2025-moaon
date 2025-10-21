aaa438742ed611cc26c78e042c078592fd229952
Minuring <mw9505@gmail.com>
2025-10-21 12:44:42 +0900

    fix: request 헤더 제거

--- backend/src/main/java/moaon/backend/s3/S3Service.java
+++ backend/src/main/java/moaon/backend/s3/S3Service.java
@@ -2,8 +2,6 @@ package moaon.backend.s3;
 
 import com.amazonaws.HttpMethod;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.Headers;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import java.net.URL;
 import java.util.Date;
@@ -35,15 +33,10 @@ public class S3Service {
     }
 
     private GeneratePresignedUrlRequest getPutGeneratePresignedUrlRequest(String fileName, Date expiration) {
-        GeneratePresignedUrlRequest generatePresignedUrlRequest
-                = new GeneratePresignedUrlRequest(bucket, fileName)
+        return new GeneratePresignedUrlRequest(bucket, fileName)
                 .withMethod(HttpMethod.PUT)
                 .withKey(fileName)
                 .withExpiration(expiration);
-        generatePresignedUrlRequest.addRequestParameter(
-                Headers.S3_CANNED_ACL,
-                CannedAccessControlList.PublicRead.toString());
-        return generatePresignedUrlRequest;
     }
 
     private Date getExpiration() {
