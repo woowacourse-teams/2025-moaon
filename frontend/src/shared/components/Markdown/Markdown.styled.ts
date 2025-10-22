cbd42424281f5a6c92411d4efaf43544d01bccd6
wo-o29 <woogur29@gmail.com>
2025-10-22 17:35:54 +0900

    refactor: 마크다운 패키지 변경

--- frontend/src/shared/components/Markdown/Markdown.styled.ts
+++ frontend/src/shared/components/Markdown/Markdown.styled.ts
@@ -2,18 +2,6 @@ import styled from "@emotion/styled";
 import Polymorphic from "../Polymorphic/Polymorphic";
 
 export const MarkdownWrapper = styled(Polymorphic)`
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  hr {
-    margin: 0;
-    margin-top: 1rem;
-    font-size: revert;
-  }
-
   blockquote {
     height: auto;
     min-height: 0;
@@ -21,49 +9,5 @@ export const MarkdownWrapper = styled(Polymorphic)`
     border-left: 4px solid #007bff;
     background-color: #f7f7f7ff;
     display: flex;
-  }
-
-  ul {
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-  }
-
-  li {
-    list-style: inside;
-  }
-
-  hr {
-    border: 1px solid #ebebebff;
-  }
-
-  table {
-    border-collapse: collapse !important;
-    width: 100% !important;
-    border: 1px solid #ddd !important;
-  }
-
-  thead {
-    background-color: #f5f5f5 !important;
-  }
-
-  th {
-    border: 1px solid #ddd !important;
-    padding: 8px !important;
-    text-align: left !important;
-    font-weight: bold !important;
-  }
-
-  td {
-    border: 1px solid #ddd !important;
-    padding: 8px !important;
-  }
-
-  tr:nth-of-type(even) {
-    background-color: #f9f9f9;
-  }
-
-  strong {
-    font-weight: 600;
   }
 `;
