290e1a55659da20605e0dd22370ab4e1565bd920
mlnwns <mlnwns@naver.com>
2025-10-23 10:48:03 +0900

    style: DropZone 컴포넌트 스타일 개선 및 정렬 속성 추가

--- frontend/src/pages/register/ProjectInfoForm/components/ImageFormField/components/FileDropZone/FileDropZone.styled.ts
+++ frontend/src/pages/register/ProjectInfoForm/components/ImageFormField/components/FileDropZone/FileDropZone.styled.ts
@@ -5,8 +5,12 @@ export const DropZone = styled.div<{ isOver: boolean; disabled: boolean }>`
   height: 9.375rem;
   border: 2px dashed ${({ isOver }) => (isOver ? "#007bff" : "#cccccc")};
   border-radius: 8px;
+  padding: 1rem;
   display: flex;
+  word-break: keep-all;
   align-items: center;
+  text-align: center;
+  line-height: 1.5;
   justify-content: center;
   background-color: ${(props) => (props.isOver ? "#e6f0ff" : "transparent")};
   cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
