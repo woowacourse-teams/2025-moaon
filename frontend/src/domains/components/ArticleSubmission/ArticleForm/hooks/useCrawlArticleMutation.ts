3f14c1bb63688a356b9a1139bc172b17c1e35e9b
jin123457 <jinsol9909@gmail.com>
2025-10-23 17:56:37 +0900

    fix: useCrawlArticleMutation에서 mutate 함수 제거 및 async 처리로 변경

--- frontend/src/domains/components/ArticleSubmission/ArticleForm/hooks/useCrawlArticleMutation.ts
+++ frontend/src/domains/components/ArticleSubmission/ArticleForm/hooks/useCrawlArticleMutation.ts
@@ -7,32 +7,7 @@ import { createEmptyFormData } from "../utils/formUtils";
 export const useCrawlArticleMutation = (
   setFormData: Dispatch<SetStateAction<ArticleFormDataType>>
 ) => {
-  const { mutate, mutateAsync, isPending } = crawlArticleQueries.fetchCrawl();
-
-  // const handleFetch = (
-  //   url: string,
-  //   disabledCondition: (remainingCount: number) => void
-  // ) => {
-  //   mutate(url, {
-  //     onSuccess: (data) => {
-  //       const { title, summary, remainingCount } = data;
-
-  //       setFormData((prev) => ({
-  //         ...prev,
-  //         ...(title ? { title } : {}),
-  //         ...(summary ? { description: summary } : {}),
-  //       }));
-
-  //       disabledCondition(remainingCount);
-  //     },
-  //     onError: (error) => {
-  //       if (error instanceof Error) {
-  //         toast.error(error.message);
-  //         return;
-  //       }
-  //     },
-  //   });
-  // };
+  const { mutateAsync, isPending } = crawlArticleQueries.fetchCrawl();
 
   const handleFetchAsync = async (
     url: string,
