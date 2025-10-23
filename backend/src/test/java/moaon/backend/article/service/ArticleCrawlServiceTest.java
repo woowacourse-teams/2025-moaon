ed57580b6f42e77481de01f056eb7633ee9d236a
Minuring <mw9505@gmail.com>
2025-10-23 23:01:11 +0900

    fix: 테스트 컴파일에러 해결

--- backend/src/test/java/moaon/backend/article/service/ArticleCrawlServiceTest.java
+++ backend/src/test/java/moaon/backend/article/service/ArticleCrawlServiceTest.java
@@ -2,23 +2,19 @@ package moaon.backend.article.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Optional;
 import moaon.backend.article.dto.ArticleCrawlResult;
 import moaon.backend.article.repository.ArticleContentRepository;
 import moaon.backend.member.domain.Member;
-import moaon.backend.member.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 
 class ArticleCrawlServiceTest {
 
-    private ArticleContentRepository repository = Mockito.mock(ArticleContentRepository.class);
-    private AiSummaryClient aiSummaryClient = Mockito.mock(AiSummaryClient.class);
-    private MemberRepository memberRepository = Mockito.mock(MemberRepository.class);
+    private final ArticleContentRepository repository = Mockito.mock(ArticleContentRepository.class);
+    private final AiSummaryClient aiSummaryClient = Mockito.mock(AiSummaryClient.class);
 
-    private ArticleCrawlService crawlService = new ArticleCrawlService(repository, aiSummaryClient, memberRepository);
+    private final ArticleCrawlService crawlService = new ArticleCrawlService(repository, aiSummaryClient);
 
     @DisplayName("하루 20번 초과 크롤링시 AI요약 없이 빈 summary를 반환한다.")
     @Test
@@ -27,9 +23,8 @@ class ArticleCrawlServiceTest {
         String url = "https://happysisyphe.tistory.com/52";
         Member member = new Member(1L, "socialId", "abc@gmail.com", "poopo", 20);
 
-        Mockito.when(memberRepository.findById(1L)).thenReturn(Optional.of(member));
         //when - then
-        ArticleCrawlResult crawlResult = crawlService.crawl(url, member.getId());
+        ArticleCrawlResult crawlResult = crawlService.crawl(url, member);
 
         assertThat(crawlResult.summary()).isEmpty();
     }
