50a2b5589b0570ea26d83fd51348c2a9ec5b4cee
Minuring <mw9505@gmail.com>
2025-10-26 18:51:08 +0900

    refactor: id리스트가 비어있으면 바로 리턴 취소

--- backend/src/main/java/moaon/backend/article/dao/ArticleDao.java
+++ backend/src/main/java/moaon/backend/article/dao/ArticleDao.java
@@ -12,7 +12,6 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
@@ -47,10 +46,6 @@ public class ArticleDao {
             ArticleSortType sortType,
             @Nullable SearchKeyword searchKeyword
     ) {
-        if (CollectionUtils.isEmpty(ids)) {
-            return new ArrayList<>();
-        }
-
         if (searchKeyword != null && searchKeyword.hasValue()) {
             return findAllWithScore(ids, cursor, limit, sortType, searchKeyword);
         }
