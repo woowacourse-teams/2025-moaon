3afec614bcef39ec546223e6f98a7b39977ea631
eueo8259 <eueo8259@gmail.com>
2025-11-04 13:27:41 +0900

    refactor: 개행 추가

--- backend/src/main/java/moaon/backend/event/repository/EsEventOutboxRepository.java
+++ backend/src/main/java/moaon/backend/event/repository/EsEventOutboxRepository.java
@@ -6,6 +6,8 @@ import moaon.backend.event.domain.EventStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EsEventOutboxRepository extends JpaRepository<EsEventOutbox, Long>, EsEventOutboxRepositoryCustom {
+
     int deleteByStatusAndProcessedAtBefore(EventStatus eventStatus, LocalDateTime cutoffTime);
+
     int deleteByStatusAndCreatedAtBefore(EventStatus eventStatus, LocalDateTime cutoffTime);
 }
