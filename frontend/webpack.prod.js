adc67632637f30f768973a41d5b4c490dd9564df
wo-o29 <woogur29@gmail.com>
2025-11-12 00:29:40 +0900

    62

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -86,21 +86,21 @@ export default {
       },
     }),
     // Service Worker 플러그인 추가
-    // new InjectManifest({
-    //   swSrc: "./src/service-worker.js", // Service Worker 소스 파일
-    //   swDest: "sw.js", // 빌드 후 출력 파일명
-    //   maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5MB
-    //   // 프리캐시할 파일 패턴
-    //   include: [
-    //     /\.html$/,
-    //     /\.js$/,
-    //     /\.css$/,
-    //     /\.png$/,
-    //     /\.svg$/,
-    //     /\.avif$/,
-    //     /\.ico$/,
-    //   ],
-    //   exclude: [/asset-manifest\.json$/, /^sw\.js$/],
-    // }),
+    new InjectManifest({
+      swSrc: "./src/service-worker.js", // Service Worker 소스 파일
+      swDest: "sw.js", // 빌드 후 출력 파일명
+      maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5MB
+      // 프리캐시할 파일 패턴
+      include: [
+        /\.html$/,
+        /\.js$/,
+        /\.css$/,
+        /\.png$/,
+        /\.svg$/,
+        /\.avif$/,
+        /\.ico$/,
+      ],
+      exclude: [/asset-manifest\.json$/, /^sw\.js$/],
+    }),
   ],
 };
