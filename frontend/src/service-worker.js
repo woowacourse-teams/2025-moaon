02e52bd605cd52bb9437dbc8b0b6e1dd76c47f29
wo-o29 <woogur29@gmail.com>
2025-11-13 02:29:39 +0900

    86

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -4,9 +4,9 @@ import {
   precacheAndRoute,
 } from "workbox-precaching";
 
-cleanupOutdatedCaches();
 // precacheAndRoute 실행
 precacheAndRoute(self.__WB_MANIFEST);
+cleanupOutdatedCaches();
 
 self.addEventListener("fetch", (event) => {
   const req = event.request;
@@ -42,19 +42,4 @@ self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    (async () => {
-      const cacheNames = await caches.keys();
-      await Promise.all(
-        cacheNames
-          .filter((cacheName) => !cacheName.startsWith("workbox-"))
-          .map((cacheName) => caches.delete(cacheName)),
-      );
-
-      await self.clients.claim();
-    })(),
-  );
 });
