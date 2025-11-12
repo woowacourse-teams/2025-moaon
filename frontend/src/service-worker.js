78c92dcd44d16e53fc68f66ed9623a2f00617cd9
wo-o29 <woogur29@gmail.com>
2025-11-13 00:07:38 +0900

    캐시 설정 수정

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -8,6 +8,9 @@ import { StaleWhileRevalidate } from "workbox-strategies";
 // precacheAndRoute 실행
 precacheAndRoute(self.__WB_MANIFEST);
 
+const APP_VERSION = process.env.APP_VERSION;
+const CACHE_NAME = `cache-v${APP_VERSION}`;
+
 // SWR 전략 + BroadcastUpdatePlugin 적용 (index.html 포함 문서, JS, CSS)
 registerRoute(
   ({ request }) =>
@@ -15,7 +18,7 @@ registerRoute(
     request.destination === "script" ||
     request.destination === "style",
   new StaleWhileRevalidate({
-    cacheName: "static-resources",
+    cacheName: CACHE_NAME,
     plugins: [
       new CacheableResponsePlugin({ statuses: [0, 200] }),
       new ExpirationPlugin({ maxEntries: 50 }),
@@ -36,7 +39,7 @@ self.addEventListener("activate", (event) => {
       const cacheNames = await caches.keys();
       await Promise.all(
         cacheNames
-          .filter((cacheName) => cacheName === "static-resources")
+          .filter((cacheName) => cacheName === CACHE_NAME)
           .map((cacheName) => caches.delete(cacheName)),
       );
       await self.clients.claim();
