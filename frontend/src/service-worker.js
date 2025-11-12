3bc456dbe2657c04a1ab124d793b403b6e15875a
wo-o29 <woogur29@gmail.com>
2025-11-13 02:11:54 +0900

    가설2

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -1,4 +1,4 @@
-import { matchPrecache, precacheAndRoute } from "workbox-precaching";
+import { getCacheKeyForURL, precacheAndRoute } from "workbox-precaching";
 
 // precacheAndRoute 실행
 precacheAndRoute(self.__WB_MANIFEST);
@@ -13,15 +13,16 @@ self.addEventListener("fetch", (event) => {
         const cache = await caches.open(
           "workbox-precache-v2-https://moaon.site/",
         );
-        const oldHTML = await matchPrecache("/index.html");
-        console.log("Old HTML:", oldHTML);
+        const oldCacheKey = getCacheKeyForURL("/index.html");
+        console.log("Old Cache Key:", oldCacheKey);
+        const oldHTML = await cache.match(oldCacheKey);
 
         const keys = await cache.keys();
         console.log(keys);
         for (const key of keys) {
-          if (key.url.includes("/index.html") && key.url !== oldHTML.url) {
+          if (key.url.includes("/index.html") && key.url !== oldCacheKey) {
             const newHTML = await cache.match(key);
-            console.log("New HTML:", newHTML);
+            console.log("New HTML:", key.url);
             if (newHTML) {
               return newHTML;
             }
