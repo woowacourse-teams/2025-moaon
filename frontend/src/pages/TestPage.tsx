1f536db8fb350d064550b327dfbf5225438c4aa3
wo-o29 <woogur29@gmail.com>
2025-11-13 00:48:58 +0900

    ㄴㄴ

--- frontend/src/pages/TestPage.tsx
+++ frontend/src/pages/TestPage.tsx
@@ -2,7 +2,7 @@ const APP_VERSION = process.env.APP_VERSION;
 
 function TestPage() {
   return (
-    <div style={{ padding: "50px" }}>
+    <div style={{ padding: "80px" }}>
       <h1 style={{ fontSize: "20px" }}>배포 ver: {APP_VERSION}</h1>
       <button type="button" onClick={() => window.location.reload()}>
         요청 보내기
--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -3,6 +3,27 @@ import { precacheAndRoute } from "workbox-precaching";
 // precacheAndRoute 실행
 precacheAndRoute(self.__WB_MANIFEST);
 
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  const url = new URL(req.url);
+  console.log(url);
+
+  // CSR 라우트 요청(일반적으로 mode가 'navigate')이면 처리
+  if (req.mode === "navigate") {
+    event.respondWith(
+      caches
+        .open("workbox-precache-v2-https://moaon.site/")
+        .then(async (cache) => {
+          console.loh(cache);
+          return cache
+            .match("/index.html", { ignoreSearch: true })
+            .then((response) => response || fetch(req));
+        }),
+    );
+    return;
+  }
+});
+
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
