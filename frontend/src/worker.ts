c4c01fb64c964263cb898c5d048c76a4ddfb1cbd
wo-o29 <woogur29@gmail.com>
2025-11-13 22:36:09 +0900

    123

--- frontend/src/worker.ts
+++ frontend/src/worker.ts
@@ -40,7 +40,7 @@ export function register(config: ServiceWorkerConfig) {
     setInterval(() => {
       console.log("checking");
       wb.update();
-    }, 1 * 60 * 1000);
+    }, 3 * 60 * 1000);
 
     return wb;
   }
