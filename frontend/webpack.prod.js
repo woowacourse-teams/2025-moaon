f668b81786775fb157d26d2d94d24a0f68c9f489
mlnwns <mlnwns@naver.com>
2026-02-27 01:59:31 +0900

    fix: webpack 설정 최적화 및 불필요한 캐시 그룹 제거

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -9,47 +9,12 @@ export default {
   },
   optimization: {
     minimize: true,
-    moduleIds: "deterministic",
     runtimeChunk: "single",
+    moduleIds: "deterministic",
     splitChunks: {
-      maxInitialRequests: 25,
-      maxAsyncRequests: 25,
-      maxSize: 200_000,
       chunks: "all",
-      cacheGroups: {
-        react: {
-          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-          name: "react",
-          priority: 30,
-        },
-        emotion: {
-          test: /[\\/]node_modules[\\/](@emotion|emotion)[\\/]/,
-          name: "emotion",
-          priority: 30,
-        },
-        tanstackQuery: {
-          test: /[\\/]node_modules[\\/](@tanstack)[\\/]/,
-          name: "tanstack-query",
-          priority: 30,
-        },
-        reactRouter: {
-          test: /[\\/]node_modules[\\/]react-router[\\/]/,
-          name: "react-router",
-          priority: 30,
-        },
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          priority: 20,
-        },
-        common: {
-          name: "common",
-          minChunks: 2,
-          reuseExistingChunk: true,
-          priority: 10,
-        },
-      },
     },
+
     minimizer: [
       new TerserPlugin({
         terserOptions: {
